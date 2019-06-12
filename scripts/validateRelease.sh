cc7ff29ae8062dbeffb3a7c793a520f5ef87d785
Wojtek Trocki <wtrocki@redhat.com>
2019-06-12 16:10:12 +0100

    fix: add private for template to not publish it (#42)

--- scripts/validateRelease.sh
+++ scripts/validateRelease.sh
@@ -33,18 +33,5 @@ for package in $PACKAGES; do
   fi
 done
 
-for package in $PACKAGES; do
-  package_dir=$(lerna --loglevel=silent ls -l | grep $package | awk -F ' ' '{print $3}')
-  package_dist_dir="${package_dir}/dist"
-  package_plugin_xml="${package_dir}/plugin.xml"
-  if [ -d "${package_dist_dir}" ]; then
-    echo "dist dir ${package_dist_dir} present"
-  elif [ -f "${package_plugin_xml}" ]; then
-    echo "dist dir ${package_dist_dir} not present but package is a Cordova plugin"
-  else
-    echo "dist dir ${package_dist_dir} not present and the package is not a Cordova plugin, possible compilation error"
-    exit 1
-  fi
-done
 
 echo "TAG and PACKAGE_VERSION are valid"
