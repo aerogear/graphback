adfb9d3c570ce2e2e9b6a4902ec073943767ba7c
ankitjena <ankitjena13@gmail.com>
2019-06-13 13:45:43 +0530

    fix: restructure init command

--- packages/graphback-cli/src/templates/templates.ts
+++ packages/graphback-cli/src/templates/templates.ts
@@ -5,13 +5,34 @@ import * as github from 'parse-github-url'
 import * as request from 'request'
 import * as tar from 'tar'
 import * as tmp from 'tmp'
-import { Template } from './template'
+import { Template } from '../templates/templateMetadata'
 
+/**
+ * available templates
+ */
+export const allTemplates: Template[] = [
+  {
+    name: 'apollo-starter-ts',
+    description: 'Basic apollo template in typescript',
+    repo: {
+      uri: 'https://github.com/aerogear/graphback',
+      branch: 'master',
+      path: '/templates/apollo-starter-ts',
+    }
+  }
+]
+/**
+ * information about repository
+ */
 interface TemplateRepositoryTarInformation {
   uri: string
   files: string
 }
 
+/**
+ * Get github repository information of template
+ * @param template template information provided
+ */
 function getTemplateRepositoryTarInformation(
   template: Template,
 ): TemplateRepositoryTarInformation {
@@ -26,17 +47,21 @@ function getTemplateRepositoryTarInformation(
   return { uri, files: template.repo.path }
 }
 
+/**
+ * download tar file from repository
+ * @param tarInfo repository info
+ */
 async function downloadRepository(
-  tar: TemplateRepositoryTarInformation,
+  tarInfo: TemplateRepositoryTarInformation,
 ): Promise<string> {
-  const spinner = ora(`Downloading starter from ${chalk.cyan(tar.uri)}`).start()
+  const spinner = ora(`Downloading starter from ${chalk.cyan(tarInfo.uri)}`).start()
   const tmpPath = tmp.fileSync({
     postfix: '.tar.gz',
   })
 
   // tslint:disable-next-line: typedef
   await new Promise((resolve) => {
-    request(tar.uri, {
+    request(tarInfo.uri, {
       headers: {
         'User-Agent': 'aerogear/graphback-cli',
       },
@@ -50,15 +75,21 @@ async function downloadRepository(
   return tmpPath.name
 }
 
+/**
+ * extract the downloaded file into the output path
+ * @param file downloaded file from repo
+ * @param repo repository information
+ * @param output output path
+ */
 async function extractStarterFromRepository(
-  tmp: string,
+  file: string,
   repo: TemplateRepositoryTarInformation,
   output: string,
 ): Promise<void> {
   const spinner = ora(`Extracting content to ${chalk.cyan(output)}`)
 
   await tar.extract({
-    file: tmp,
+    file: file,
     cwd: output,
     filter: (path: string) => RegExp(repo.files).test(path),
     strip: repo.files.split('/').length,
@@ -69,9 +100,14 @@ async function extractStarterFromRepository(
   return
 }
 
+/**
+ * download and extract template from repository into project folder
+ * @param template template information
+ * @param name name of project folder
+ */
 export async function extractTemplate(template: Template, name: string) {
-  const tar = getTemplateRepositoryTarInformation(template)
-  const tmp = await downloadRepository(tar)
+  const tarInfo = getTemplateRepositoryTarInformation(template)
+  const file = await downloadRepository(tarInfo)
   const output = `${process.cwd()}/${name}`
-  await extractStarterFromRepository(tmp, tar, output)
+  await extractStarterFromRepository(file, tarInfo, output)
 }
---
+++ packages/graphback-cli/src/utils/initHelper.ts
@@ -0,0 +1,106 @@
+import * as execa from 'execa'
+import { accessSync, mkdirSync } from 'fs'
+import { prompt as ask } from 'inquirer'
+import ora from 'ora'
+import { Template } from '../templates/templateMetadata'
+import { allTemplates, extractTemplate } from '../templates/templates'
+import { logError, logInfo } from './index'
+import chalk from 'chalk';
+
+/**
+ * Install dependencies, currently only npm
+ * @param name project folder name
+ */
+async function installDependencies(name: string): Promise<void> {
+  process.chdir(name)
+  const spinner = ora('Installing dependencies').start()
+  await execa('npm', ['i'])
+  spinner.succeed()
+}
+
+/**
+ * Check if directory exists
+ * @param path path of the directory
+ * @param name name of the project folder
+ */
+async function checkDirectory(path: string, name: string): Promise<void> {
+  try {
+    accessSync(path)
+    logError(`A folder with name ${name} exists. Remove it or try another name.`)
+    process.exit(0)
+  } catch (error) {
+    return
+  }
+}
+
+/**
+ * choose a template from available templates
+ */
+async function chooseTemplate(): Promise<Template> {
+  const { templateName } = await ask([
+    {
+      type: 'list',
+      name: 'templateName',
+      message: 'Choose a template to bootstrap',
+      choices: allTemplates.map((t: Template) => t.name)
+    }
+  ])
+
+  return allTemplates.find((t: Template) => t.name === templateName)
+}
+
+/**
+ * check if template name is valid or not
+ * @param templateName name of the template provided
+ */
+function checkTemplateName(templateName: string): void {
+  const availableTemplates = allTemplates.map((t: Template) => t.name)
+  if(availableTemplates.includes(templateName)) {
+    return 
+  }
+  logError("Template with given name doesn't exist. Give one of available ones or simply choose by not providing a template name")
+  process.exit(0)
+}
+
+/**
+ * assign template details from the given input or choice
+ * @param templateName name of the template provided(if any)
+ */
+async function assignTemplate(templateName: string): Promise<Template> {
+  let template
+  if(templateName) {
+    checkTemplateName(templateName)
+    template = allTemplates.find((t: Template) => t.name === templateName)
+  } else {
+    template = await chooseTemplate()
+  }
+
+  return template
+}
+
+function postSetupMessage(name: string): string {
+  return `
+  GraphQL server successfully bootstrapped :rocket:
+  Next Steps:
+  1. change directory into project folder - ${chalk.cyan(`cd ${name}`)}
+  2. Create a graphql file inside ${chalk.cyan(`model`)} containing your Types
+  3. run ${chalk.cyan(`graphback build`)} to generate schema and resolvers
+  4. run ${chalk.cyan(`graphback db`)} to create database resources in postgres
+  `
+}
+
+/**
+ * init command handler
+ * @param name name of project folder
+ * @param templateName name of the template provided(if any)
+ */
+export async function init(name: string, templateName?: string) {
+  const path = `${process.cwd()}/${name}`
+  await checkDirectory(path, name)
+  const template = await assignTemplate(templateName)
+  mkdirSync(path)
+  logInfo(`Bootstraping graphql server :dizzy: :sparkles:`)
+  await extractTemplate(template, name)
+  await installDependencies(name)
+  logInfo(postSetupMessage(name))
+}
