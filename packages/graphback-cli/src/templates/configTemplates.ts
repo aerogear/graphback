8deb938dcbfa2454d2c7127f970dffd085860ea3
ankitjena <ankitjena13@gmail.com>
2019-06-20 23:49:53 +0530

    fix: create separate files for config

--- packages/graphback-cli/src/templates/configTemplates.ts
+++ packages/graphback-cli/src/templates/configTemplates.ts
@@ -1,41 +1,19 @@
-import { writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+
+const configFilesPath = `${__dirname}/resources/config`
 
 /**
  * Different database choices
  */
 export const defaultDB = {
   name: 'postgres',
-  config: (projectName: string) => {
-    return {
-      'user': 'postgresql',
-      'password': 'postgres',
-      'database': `${projectName}`,
-      'host': '127.0.0.1',
-      'port': '5432'
-    }
-  }
+  config: readFileSync(`${configFilesPath}/pgconfig.json`, 'utf8')
 }
 
-const pgDockerCompose = (projectName: string) => {
-  return `version: '3'
+const dockerFilesPath = `${__dirname}/resources/docker`
 
-services:
-  postgres:
-    image: postgres:9.6
-    ports:
-      - "5432:5432"
-    environment:
-      POSTGRES_PASSWORD: postgres
-      POSTGRES_USER: postgresql
-      POSTGRES_DB: ${projectName}
-
-  # Mosca is a simple MQTT Broker
-  # In OpenShift/Production we would use the Red Hat AMQ broker
-  mosca:
-    image: matteocollina/mosca
-    ports:
-      - "1883:1883" # MQTT
-      - "80:80" # web interface`
+const pgDockerCompose = () => {
+  return readFileSync(`${dockerFilesPath}/pg-docker-compose.yml`, 'utf8')
 }
 
 /**
@@ -45,8 +23,8 @@ export const createDBConfig = (projectName: string) => {
   const configPath = `${process.cwd()}/config.json`
   const dockerComposePath = `${process.cwd()}/docker-compose.yml`
   const config = {
-    dbConfig: defaultDB.config(projectName)
+    dbConfig: defaultDB.config
   }
   writeFileSync(configPath, JSON.stringify(config, undefined, 2))
-  writeFileSync(dockerComposePath, pgDockerCompose(projectName))
+  writeFileSync(dockerComposePath, pgDockerCompose())
 }
