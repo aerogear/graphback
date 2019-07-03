e0402b2f7c413397a66274127991c8fae15798ea
ankitjena <ankitjena13@gmail.com>
2019-07-03 21:57:45 +0530

    fix: more info in context, unify template

--- packages/graphback-schema/src/context.ts
+++ packages/graphback-schema/src/context.ts
@@ -7,7 +7,10 @@ export interface Context {
   types: string[]
   nodes: Definition[]
   inputFields: Definition[]
-  filterFields: Definition[]
+  filterFields: Definition[]  
+  queries: string[]
+  mutations: string[]
+  subscriptions: string[]
 }
 
 const scalars = ['ID', 'String', 'Boolean', 'Int', 'Float']
@@ -28,12 +31,63 @@ const inputFields = (fields: string[]): string[] => {
   return fields.filter((f: string) => !f.startsWith('id'))
 }
 
+enum Crud {
+  CREATE = "create",
+  UPDATE = "update",
+  DELETE = "delete",
+  FIND = "find",
+  FIND_ALL = "findAll"
+}
+
+const getFieldName = (typeName: string, action: Crud, plural: string = ''): string => {
+  const upperCasedType = typeName.charAt(0).toUpperCase() + typeName.substr(1);
+
+  return `${action}${upperCasedType}${plural}`
+}
+
+const find = (name: string): string => {
+  const fieldName = getFieldName(name, Crud.FIND)
+  
+  return `${fieldName}(fields: ${name}Filter!): ${name}!`
+}
+
+const update = (name: string): string => {
+  const fieldName = getFieldName(name, Crud.UPDATE)
+
+  return `${fieldName}(id: ID!, input: ${name}Input!): ${name}!`
+}
+
+const create = (name: string): string => {
+  const fieldName = getFieldName(name, Crud.CREATE)
+  
+  return `${fieldName}(input: ${name}Input!): ${name}!`
+}
+
+const del = (name: string): string => {
+  const fieldName = getFieldName(name, Crud.DELETE)
+  
+  return `${fieldName}(id: ID!): ID!`
+}
+
+const findAll = (name: string): string => {
+  const fieldName = getFieldName(name, Crud.FIND_ALL)
+  
+  return `${fieldName}: ${name}Pagination`
+}
+
+const newSub = (name: string) => `new${name}: ${name}!`
+const updatedSub = (name: string) => `updated${name}: ${name}!`
+const deletedSub = (name: string) => `deleted${name}: ID!`
+
 export const buildContext = (definitions: Definition[]) => {
   const context: Context = {
     types: [],
     nodes: [],
     inputFields: [],
-    filterFields: []
+    filterFields: [],
+    queries: [],
+    mutations: [],
+    subscriptions: []
   }
   context.types = definitions.map((d: Definition) => d.name)
   context.nodes = definitions.map((d: Definition) => {
@@ -54,6 +108,9 @@ export const buildContext = (definitions: Definition[]) => {
       "fields": validateFields(d.fields).map((s: string) => s.replace('!',''))
     }
   })
+  context.queries = [...context.types.map(find), ...context.types.map(findAll)]
+  context.mutations = [...context.types.map(create), ...context.types.map(update), ...context.types.map(del)]
+  context.subscriptions = [...context.types.map(newSub), ...context.types.map(updatedSub), ...context.types.map(deletedSub)]
 
   return context
 }
