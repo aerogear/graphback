84743d8383ee5427ad38c047b1fe0b537911d20f
ankitjena <ankitjena13@gmail.com>
2019-07-03 21:57:45 +0530

    fix: more info in context, unify template

--- packages/graphback-schema/src/schema.ts
+++ packages/graphback-schema/src/schema.ts
@@ -1,24 +1,5 @@
 import { Context, Definition } from './context';
 
-enum Crud {
-  CREATE = "create",
-  UPDATE = "update",
-  DELETE = "delete",
-  FIND = "find",
-  FIND_ALL = "findAll"
-}
-
-const getFieldName = (typeName: string, action: Crud, plural: string = ''): string => {
-  const upperCasedType = typeName.charAt(0).toUpperCase() + typeName.substr(1);
-
-  return `${action}${upperCasedType}${plural}`
-}
-
-const node = `interface Node {
-  id: ID!
-}
-`
-
 const pagination = (types: string[]): string => {
   return `type PaginationInfo {
   totalPages: Int!
@@ -32,94 +13,52 @@ const pagination = (types: string[]): string => {
 ${types.map((name: string) => `type ${name}Pagination {
   items: [${name}]!
   pageInfo: PaginationInfo!
-}
-`).join('\n')}
-`
+}`).join('\n\n')}`
 }
 
 const inputs = (defs: Definition[]): string => {
   return `${defs.map((d: Definition) => `input ${d.name}Input {
   ${d.fields.join('\n  ')}
-}`).join('\n\n')}
-`
+}`).join('\n\n')}`
 }
 
 const nodeTypes = (defs: Definition[]): string => {
   return `${defs.map((d: Definition) => `type ${d.name} implements Node {
   ${d.fields.join('\n  ')}
-}`).join('\n\n')}
-`
+}`).join('\n\n')}`
 }
 
 const filters = (defs: Definition[]): string => {
   return `${defs.map((d: Definition) => `type ${d.name}Filter {
   ${d.fields.join('\n  ')}
-}`).join('\n\n')}
-` 
+}`).join('\n\n')}` 
 }
 
-
-const find = (name: string): string => {
-  const fieldName = getFieldName(name, Crud.FIND)
-  
-  return `${fieldName}(fields: ${name}Filter!): ${name}!`
+const outputSchema = (context: Context): string =>  `interface Node {
+  id: ID!
 }
 
-const update = (name: string): string => {
-  const fieldName = getFieldName(name, Crud.UPDATE)
+${inputs(context.inputFields)}
 
-  return `${fieldName}(id: ID!, input: ${name}Input!): ${name}!`
+${nodeTypes(context.nodes)}
+
+${filters(context.filterFields)}
+
+${pagination(context.types)}
+
+type Query {
+  ${context.queries.join('\n  ')}
 }
 
-const create = (name: string): string => {
-  const fieldName = getFieldName(name, Crud.CREATE)
-  
-  return `${fieldName}(input: ${name}Input!): ${name}!`
+type Mutation {
+  ${context.mutations.join('\n  ')}
 }
 
-const del = (name: string): string => {
-  const fieldName = getFieldName(name, Crud.DELETE)
-  
-  return `${fieldName}(id: ID!): ID!`
-}
-
-const findAll = (name: string): string => {
-  const fieldName = getFieldName(name, Crud.FIND_ALL)
-  
-  return `${fieldName}: ${name}Pagination`
-}
-
-const query = (types: string[]): string => {
-  return `type Query {
-  ${types.map(find).join('\n  ')}
-  ${types.map(findAll).join('\n  ')}
-}
-  `
-}
-
-const mutations = (types: string[]): string => {
-  return `type Mutation {
-  ${types.map(create).join('\n  ')}
-  ${types.map(update).join('\n  ')}
-  ${types.map(del).join('\n  ')}
+type Subscription {
+  ${context.subscriptions.join('\n  ')}
 }
 `
-}
-
-const newSub = (name: string) => `new${name}: ${name}!`
-const updatedSub = (name: string) => `updated${name}: ${name}!`
-const deletedSub = (name: string) => `deleted${name}: ID!`
-
-const subscriptions = (types: string[]): string => {
-  return `type Subscription {
-  ${types.map(newSub).join('\n  ')}
-  ${types.map(updatedSub).join('\n  ')}
-  ${types.map(deletedSub).join('\n  ')}
-}`
-}
 
 export const generateSchema = (context: Context): string => {
-  const { inputFields, nodes, filterFields, types} = context
-  
-  return [node, inputs(inputFields), nodeTypes(nodes), filters(filterFields), pagination(types), query(types), mutations(types), subscriptions(types)].join('\n')
+  return outputSchema(context)
 }
