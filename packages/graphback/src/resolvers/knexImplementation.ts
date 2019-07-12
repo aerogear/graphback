837eebf139ed3f9c9e1c8b581aadcbf2d04bc116
ankitjena <ankitjena13@gmail.com>
2019-07-13 00:10:01 +0530

    fix: resolver implementation

--- packages/graphback/src/resolvers/knexImplementation.ts
+++ packages/graphback/src/resolvers/knexImplementation.ts
@@ -1,29 +1,32 @@
-export const createTemplate = (fieldName: string, tableName: string, knexContext: string, argumentContext: string): string => {
+const knexContext = `context.db`
+const argumentContext = `args`
+
+export const createTemplate = (fieldName: string, tableName: string): string => {
     return `${fieldName}: (root, args, context, info) => {
       return ${knexContext}('${tableName}').insert(${argumentContext}.input).returning('*')
     }`
   }
 
-export const deleteTemplate = (fieldName: string, tableName: string, knexContext: string, argumentContext: string): string => {
+export const deleteTemplate = (fieldName: string, tableName: string): string => {
     return `${fieldName}: (root, args, context, info) => {
       return ${knexContext}('${tableName}').where('id', '=' , ${argumentContext}.id).del().then( () => {
       return ${argumentContext}.id;
     })}`
 }
 
-export const findAllTemplate = (fieldName: string, tableName: string, knexContext: string, argumentContext: string): string => {
+export const findAllTemplate = (fieldName: string, tableName: string): string => {
     return `${fieldName}: (root, args, context, info) => {
       return ${knexContext}.select().from('${tableName}')
     }`
   }
 
-export const findTemplate = (fieldName: string, tableName: string, knexContext: string, argumentContext: string): string => {
+export const findTemplate = (fieldName: string, tableName: string): string => {
     return `${fieldName}: (root, args, context, info) => {
-      return ${knexContext}.select().from('${tableName}').where(${argumentContext}.field.name, '=', ${argumentContext}.field.value)
+      return ${knexContext}.select().from('${tableName}').where(...${argumentContext}.fields)
     }`
 }
 
-export const updateTemplate = (fieldName: string, tableName: string, knexContext: string, argumentContext: string): string => {
+export const updateTemplate = (fieldName: string, tableName: string): string => {
     return `${fieldName}: (root, args, context, info) => {
       return ${knexContext}('${tableName}').where('id', '=' , ${argumentContext}.id).update(${argumentContext}.input).then( () => {
         return ${knexContext}.select().from('${tableName}').where('id', '=' , ${argumentContext}.id);
--- packages/graphback/src/resolvers/targetResolverContext.ts
+++ packages/graphback/src/resolvers/targetResolverContext.ts
@@ -9,27 +9,27 @@ export interface TargetResolverContext {
 
 const createResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.create)
-                .map((i: Type) => knex.createTemplate(getFieldName(i.name, ResolverType.CREATE), getTableName(i.name), 'db', 'args'))
+                .map((i: Type) => knex.createTemplate(getFieldName(i.name, ResolverType.CREATE), getTableName(i.name)))
 }
 
 const updateResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.update)
-                .map((i: Type) => knex.updateTemplate(getFieldName(i.name, ResolverType.UPDATE), getTableName(i.name), 'db', 'args'))
+                .map((i: Type) => knex.updateTemplate(getFieldName(i.name, ResolverType.UPDATE), getTableName(i.name)))
 }
 
 const deleteResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.delete)
-                .map((i: Type) => knex.deleteTemplate(getFieldName(i.name, ResolverType.DELETE), getTableName(i.name), 'db', 'args'))
+                .map((i: Type) => knex.deleteTemplate(getFieldName(i.name, ResolverType.DELETE), getTableName(i.name)))
 }
 
 const findResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.find)
-                .map((i: Type) => knex.findTemplate(getFieldName(i.name, ResolverType.FIND), getTableName(i.name), 'db', 'args'))
+                .map((i: Type) => knex.findTemplate(getFieldName(i.name, ResolverType.FIND), getTableName(i.name)))
 }
 
 const findAllResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.findAll)
-                .map((i: Type) => knex.findAllTemplate(getFieldName(i.name, ResolverType.FIND_ALL), getTableName(i.name), 'db', 'args'))
+                .map((i: Type) => knex.findAllTemplate(getFieldName(i.name, ResolverType.FIND_ALL, 's'), getTableName(i.name)))
 }
 
 export const buildResolverTargetContext = (inputContext: Type[]): TargetResolverContext => {
