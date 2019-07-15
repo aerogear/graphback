93b01a536effb3069f5f54c93eecd5e65fab4590
ankitjena <ankitjena13@gmail.com>
2019-07-16 00:22:28 +0530

    feat: add subscription resolvers

--- packages/graphback/src/resolvers/knex/targetResolverContext.ts
+++ packages/graphback/src/resolvers/knex/targetResolverContext.ts
@@ -5,21 +5,23 @@ import * as knex from './resolverImplementation'
 export interface TargetResolverContext {
   queries: string[]
   mutations: string[]
+  subscriptions: string[],
+  subscriptionTypes: string[]
 }
 
 const createResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.create)
-                .map((i: Type) => knex.createTemplate(getFieldName(i.name, ResolverType.CREATE), getTableName(i.name)))
+                .map((i: Type) => knex.createTemplate(getFieldName(i.name, ResolverType.CREATE), getTableName(i.name), i.name))
 }
 
 const updateResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.update)
-                .map((i: Type) => knex.updateTemplate(getFieldName(i.name, ResolverType.UPDATE), getTableName(i.name)))
+                .map((i: Type) => knex.updateTemplate(getFieldName(i.name, ResolverType.UPDATE), getTableName(i.name), i.name))
 }
 
 const deleteResolvers = (context: Type[]): string[] => {
   return context.filter((i: Type) => i.config.delete)
-                .map((i: Type) => knex.deleteTemplate(getFieldName(i.name, ResolverType.DELETE), getTableName(i.name)))
+                .map((i: Type) => knex.deleteTemplate(getFieldName(i.name, ResolverType.DELETE), getTableName(i.name), i.name))
 }
 
 const findResolvers = (context: Type[]): string[] => {
@@ -32,14 +34,39 @@ const findAllResolvers = (context: Type[]): string[] => {
                 .map((i: Type) => knex.findAllTemplate(getFieldName(i.name, ResolverType.FIND_ALL, 's'), getTableName(i.name)))
 }
 
+const newSubs = (context: Type[]): string[] => {
+  return context.filter((i: Type) => i.config.create)
+                .map((i: Type) => knex.newSub(i.name))
+}
+
+const updatedSubs = (context: Type[]): string[] => {
+  return context.filter((i: Type) => i.config.update)
+                .map((i: Type) => knex.updatedSub(i.name))
+}
+
+const deletedSubs = (context: Type[]): string[] => {
+  return context.filter((i: Type) => i.config.delete)
+                .map((i: Type) => knex.deletedSub(i.name))
+}
+
+const createSubscriptionTypes = (context: Type[]): string[] => {
+  return context.map((i: Type) => `NEW_${i.name.toUpperCase()} = 'new${i.name.toLowerCase()}',
+  UPDATED_${i.name.toUpperCase()} = 'updated${i.name.toLowerCase()}',
+  DELETED_${i.name.toUpperCase()} = 'deleted${i.name.toLowerCase()}'`)
+}
+
 export const buildResolverTargetContext = (inputContext: Type[]): TargetResolverContext => {
   const context = {
     queries: [],
-    mutations: []
+    mutations: [],
+    subscriptions: [],
+    subscriptionTypes: []
   }
 
   context.queries = [...findResolvers(inputContext), ...findAllResolvers(inputContext)]
   context.mutations = [...createResolvers(inputContext), ...updateResolvers(inputContext), ...deleteResolvers(inputContext)]
+  context.subscriptions = [...newSubs(inputContext), ...updatedSubs(inputContext), ...deletedSubs(inputContext)]
+  context.subscriptionTypes = createSubscriptionTypes(inputContext)
 
   return context
 }
--- packages/graphback/src/resolvers/resolverTemplate.ts
+++ packages/graphback/src/resolvers/resolverTemplate.ts
@@ -1,13 +1,26 @@
 import { TargetResolverContext } from './knex/targetResolverContext';
 
+const imports = `import { pubsub } from '../src/subscriptions'`
+
 export const generateResolvers = (context: TargetResolverContext): string => {
-  return `export const resolvers = {
+  return `${imports}
+
+enum Subscriptions {
+  ${context.subscriptionTypes.join(',\n  ')}
+}
+
+export const resolvers = {
   Query: {
     ${context.queries.join(',\n    ')}
   },
 
   Mutation: {
     ${context.mutations.join(',\n    ')}
+  },
+
+  Subscription: {
+    ${context.subscriptions.join(',\n    ')}
   }
-}`
+}
+`
 }
