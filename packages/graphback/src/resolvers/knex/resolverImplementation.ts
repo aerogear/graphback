36b9545e9cbf4d12e09be7f1d39f5e4fbaf54a18
ankitjena <ankitjena13@gmail.com>
2019-07-16 11:17:46 +0530

    fix: pass pubsub through context

--- packages/graphback/src/resolvers/knex/resolverImplementation.ts
+++ packages/graphback/src/resolvers/knex/resolverImplementation.ts
@@ -1,9 +1,10 @@
 const knexContext = `context.db`
+const pubsub = `context.pubsub`
 
 export const createTemplate = (fieldName: string, tableName: string, typeName: string): string => {
-    return `${fieldName}: async (root, args, context, info) => {
+    return `${fieldName}: async (_, args, context) => {
       const result = await ${knexContext}('${tableName}').insert(args.input).returning('*')
-      pubsub.publish(Subscriptions.NEW_${typeName.toUpperCase()}, {
+      ${pubsub}.publish(Subscriptions.NEW_${typeName.toUpperCase()}, {
         new${typeName}: result[0]
       })
       return result[0]
@@ -11,9 +12,9 @@ export const createTemplate = (fieldName: string, tableName: string, typeName: s
   }
 
 export const deleteTemplate = (fieldName: string, tableName: string, typeName: string): string => {
-    return `${fieldName}: (root, args, context, info) => {
+    return `${fieldName}: (_, args, context) => {
       return ${knexContext}('${tableName}').where('id', '=' , args.id).del().then( () => {
-        pubsub.publish(Subscriptions.DELETED_${typeName.toUpperCase()}, {
+        ${pubsub}.publish(Subscriptions.DELETED_${typeName.toUpperCase()}, {
           deleted${typeName}: args.id
         })
         return args.id;
@@ -21,22 +22,22 @@ export const deleteTemplate = (fieldName: string, tableName: string, typeName: s
 }
 
 export const findAllTemplate = (fieldName: string, tableName: string): string => {
-    return `${fieldName}: (root, args, context, info) => {
+    return `${fieldName}: (_, __, context) => {
       return ${knexContext}.select().from('${tableName}')
     }`
   }
 
 export const findTemplate = (fieldName: string, tableName: string): string => {
-    return `${fieldName}: (root, args, context, info) => {
+    return `${fieldName}: (_, args, context) => {
       return ${knexContext}.select().from('${tableName}').where(args.fields)
     }`
 }
 
 export const updateTemplate = (fieldName: string, tableName: string, typeName: string): string => {
-    return `${fieldName}: (root, args, context, info) => {
+    return `${fieldName}: (_, args, context) => {
       return ${knexContext}('${tableName}').where('id', '=' , args.id).update(args.input).then( async () => {
         const result = await ${knexContext}.select().from('${tableName}').where('id', '=' , args.id);
-        pubsub.publish(Subscriptions.UPDATED_${typeName.toUpperCase()}, {
+        ${pubsub}.publish(Subscriptions.UPDATED_${typeName.toUpperCase()}, {
           updated${typeName}: result[0]
         })
         return result[0]
@@ -45,18 +46,18 @@ export const updateTemplate = (fieldName: string, tableName: string, typeName: s
 
 export const newSub = (typeName: string): string => {
     return `new${typeName}: {
-      subscribe: () => pubsub.asyncIterator(Subscriptions.NEW_${typeName.toUpperCase()})
+      subscribe: (_, __, context) => ${pubsub}.asyncIterator(Subscriptions.NEW_${typeName.toUpperCase()})
     }`
 }
 
 export const updatedSub = (typeName: string): string => {
     return `updated${typeName}: {
-      subscribe: () => pubsub.asyncIterator(Subscriptions.UPDATED_${typeName.toUpperCase()})
+      subscribe: (_, __, context) => ${pubsub}.asyncIterator(Subscriptions.UPDATED_${typeName.toUpperCase()})
     }`
 }
 
 export const deletedSub = (typeName: string): string => {
     return `deleted${typeName}: {
-      subscribe: () => pubsub.asyncIterator(Subscriptions.DELETED_${typeName.toUpperCase()})
+      subscribe: (_, __, context) => ${pubsub}.asyncIterator(Subscriptions.DELETED_${typeName.toUpperCase()})
     }`
 }
