4e8a45ef36768399e893521e7ba17033443075be
ankitjena <ankitjena13@gmail.com>
2019-07-17 19:46:41 +0530

    fix: rename context interface

--- packages/graphback/src/resolvers/knex/resolverImplementation.ts
+++ packages/graphback/src/resolvers/knex/resolverImplementation.ts
@@ -2,7 +2,7 @@ const knexContext = `context.db`
 const pubsub = `context.pubsub`
 
 export const createTemplate = (fieldName: string, tableName: string, typeName: string): string => {
-    return `${fieldName}: async (_: any, args: any, context: Context) => {
+    return `${fieldName}: async (_: any, args: any, context: GraphQLContext) => {
       const result = await ${knexContext}('${tableName}').insert(args.input).returning('*')
       ${pubsub}.publish(Subscriptions.NEW_${typeName.toUpperCase()}, {
         new${typeName}: result[0]
@@ -12,7 +12,7 @@ export const createTemplate = (fieldName: string, tableName: string, typeName: s
   }
 
 export const deleteTemplate = (fieldName: string, tableName: string, typeName: string): string => {
-    return `${fieldName}: (_: any, args: any, context: Context) => {
+    return `${fieldName}: (_: any, args: any, context: GraphQLContext) => {
       return ${knexContext}('${tableName}').where('id', '=' , args.id).del().then( () => {
         ${pubsub}.publish(Subscriptions.DELETED_${typeName.toUpperCase()}, {
           deleted${typeName}: args.id
@@ -22,19 +22,19 @@ export const deleteTemplate = (fieldName: string, tableName: string, typeName: s
 }
 
 export const findAllTemplate = (fieldName: string, tableName: string): string => {
-    return `${fieldName}: (_: any, __: any, context: Context) => {
+    return `${fieldName}: (_: any, __: any, context: GraphQLContext) => {
       return ${knexContext}.select().from('${tableName}')
     }`
   }
 
 export const findTemplate = (fieldName: string, tableName: string): string => {
-    return `${fieldName}: (_: any, args: any, context: Context) => {
+    return `${fieldName}: (_: any, args: any, context: GraphQLContext) => {
       return ${knexContext}.select().from('${tableName}').where(args.fields)
     }`
 }
 
 export const updateTemplate = (fieldName: string, tableName: string, typeName: string): string => {
-    return `${fieldName}: (_: any, args: any, context: Context) => {
+    return `${fieldName}: (_: any, args: any, context: GraphQLContext) => {
       return ${knexContext}('${tableName}').where('id', '=' , args.id).update(args.input).then( async () => {
         const result = await ${knexContext}.select().from('${tableName}').where('id', '=' , args.id);
         ${pubsub}.publish(Subscriptions.UPDATED_${typeName.toUpperCase()}, {
@@ -46,18 +46,24 @@ export const updateTemplate = (fieldName: string, tableName: string, typeName: s
 
 export const newSub = (typeName: string): string => {
     return `new${typeName}: {
-      subscribe: (_: any, __: any, context: Context) => ${pubsub}.asyncIterator(Subscriptions.NEW_${typeName.toUpperCase()})
+      subscribe: (_: any, __: any, context: GraphQLContext) => {
+        return ${pubsub}.asyncIterator(Subscriptions.NEW_${typeName.toUpperCase()})
+      }
     }`
 }
 
 export const updatedSub = (typeName: string): string => {
     return `updated${typeName}: {
-      subscribe: (_: any, __: any, context: Context) => ${pubsub}.asyncIterator(Subscriptions.UPDATED_${typeName.toUpperCase()})
+      subscribe: (_: any, __: any, context: GraphQLContext) => {
+        return ${pubsub}.asyncIterator(Subscriptions.UPDATED_${typeName.toUpperCase()})
+      }
     }`
 }
 
 export const deletedSub = (typeName: string): string => {
     return `deleted${typeName}: {
-      subscribe: (_: any, __: any, context: Context) => ${pubsub}.asyncIterator(Subscriptions.DELETED_${typeName.toUpperCase()})
+      subscribe: (_: any, __: any, context: GraphQLContext) => {
+        return ${pubsub}.asyncIterator(Subscriptions.DELETED_${typeName.toUpperCase()})
+      }
     }`
 }
--- templates/apollo-starter-ts/src/context.ts
+++ templates/apollo-starter-ts/src/context.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import knex from 'knex'
 
-export interface Context {
+export interface GraphQLContext {
   pubsub: RedisPubSub
   req: express.Request
   db: knex<any, unknown[]>
