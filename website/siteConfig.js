f4c9f7317b44f8620e0bf98c5b2ef633e93a56d3
ankitjena <ankitjena13@gmail.com>
2019-07-19 13:18:10 +0530

    fix: navbar

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'getting-started', label: 'Documentation'},
-    {page: 'help', label: 'Help'},
+    // {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
 
