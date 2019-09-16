dbeb64d4f94007f18d4df4fc051bc66d788bf4ae
Wojtek Trocki <wtrocki@redhat.com>
2019-09-16 17:14:39 +0100

    fix: remove graphql-js starter (#339)

--- templates/graphql-js-starter/src/context.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import knex from 'knex'
-
-export interface GraphQLContext {
-  pubsub: any
-  req: express.Request
-  db: knex<any, unknown[]>
-}
--- templates/graphql-js-starter/src/db.ts
+++
@@ -1,9 +0,0 @@
-import knex from 'knex'
-import * as config from '../config.json'
-
-export async function connect(options: knex.MySqlConnectionConfig) {
-  return knex({
-    client: config.database,
-    connection: options
-  })
-}
