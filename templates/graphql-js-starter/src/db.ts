722c27ddaa9b330bf20bfad869dfb7092d9d80aa
Wojtek Trocki <wtrocki@redhat.com>
2019-09-16 17:14:39 +0100

    fix: remove graphql-js starter (#339)

--- templates/graphql-js-starter/src/db.ts
+++
@@ -1,9 +0,0 @@
-import knex from 'knex'
-import * as config from '../graphback.json'
-
-export async function connect(options: knex.MySqlConnectionConfig) {
-  return knex({
-    client: config.database,
-    connection: options
-  })
-}
--- templates/graphql-js-starter/src/subscriptions.ts
+++
@@ -1,7 +0,0 @@
-import { RedisPubSub } from 'graphql-redis-subscriptions'
-import Redis from 'ioredis'
-
-export const pubsub = new RedisPubSub({
-  publisher: new Redis(6789),
-  subscriber: new Redis(6789)
-})
