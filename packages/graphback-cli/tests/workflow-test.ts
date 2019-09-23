22e0d1cfbfc6779d33b866e097030604e2a40f9b
Wojciech Trocki <wtrocki@redhat.com>
2019-09-23 15:59:36 +0100

    fix: paths issue

--- packages/graphback-cli/tests/workflow-test.ts
+++ packages/graphback-cli/tests/workflow-test.ts
@@ -2,7 +2,7 @@ import ava, { ExecutionContext } from 'ava';
 // tslint:disable-next-line: no-var-requires
 const execa = require('execa');
 import { existsSync } from 'fs';
-import { resolve } from 'path';
+import { join, resolve } from 'path';
 import { createDB, generate, initConfig } from '../src';
 
 const model = {
@@ -23,11 +23,11 @@ ava('Test cli workflow', async (t: ExecutionContext) => {
   await generate();
   await createDB();
 
-  t.true(existsSync(`${basePath}/graphback.json`))
-  t.true(existsSync(`${basePath}/db.sqlite`))
-  t.true(existsSync(`${basePath}/client/src/graphql/fragments/Test.ts`))
-  t.true(existsSync(`${basePath}/src/resolvers/generated/Test.ts`))
-  t.true(existsSync(`${basePath}/src/schema/generated.ts`))
+  t.true(existsSync(join(basePath,"graphback.json")))
+  t.true(existsSync(join(basePath,"db.sqlite")))
+  t.true(existsSync(join(basePath,"client/src/graphql/fragments/Test.ts")))
+  t.true(existsSync(join(basePath,"src/schema/generated.ts")))
+  t.true(existsSync(join(basePath,"src/resolvers/generated/test.ts")))
   try {
     await execa('npm', ['run', 'build']);
   } catch (error) {
