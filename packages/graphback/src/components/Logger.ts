36fa56556fb231b48eac93bc38ebcf4e01eb6fd5
Wojciech Trocki <wtrocki@redhat.com>
2019-10-08 17:44:29 +0100

    feat: Add example component implementation

---
+++ packages/graphback/src/components/Logger.ts
@@ -0,0 +1,10 @@
+
+
+/**
+ * Loggger component that provides audit log information
+ * TODO - This is just an example component that can be used in service layer
+ */
+export const logger = (message: string) => {
+    // tslint:disable-next-line: no-console
+    console.log(message);
+};
