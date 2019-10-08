ff57d65cd22acc870e7121249d9483b60d15d57c
Wojciech Trocki <wtrocki@redhat.com>
2019-10-08 16:49:30 +0100

    feat: Support different schema formats in schema gen

--- packages/graphback/src/generators/schema/index.ts
+++ packages/graphback/src/generators/schema/index.ts
@@ -1,27 +1,2 @@
-import { Type } from '../../input/ContextTypes';
-import { generateSchema } from './schemaTemplate';
-import { buildTargetContext, createCustomSchemaContext, TargetContext } from './targetSchemaContext';
-
-
-/**
- * generate schema using graphql-codegen and visitor pattern
- * using string templates
- */
-export class SchemaGenerator {
-  private context: TargetContext
-  private inputContext: Type[]
-
-  constructor(inputContext: Type[]) {
-    this.inputContext = inputContext
-  }
-
-  /**
-   * Generate output schema as string
-   */
-  public generate() {
-    this.context = buildTargetContext(this.inputContext)
-    const [ customQueries, customMutations, customSubscriptions ] = createCustomSchemaContext(this.inputContext)
-
-    return generateSchema(this.context, customQueries, customMutations, customSubscriptions)
-  }
-}
+export * from './SchemaGenerator'
+export * from './schemaFormatters'
