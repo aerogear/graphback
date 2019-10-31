ec023c37c626fd66fb87c980ea289d254bdd9c41
Wojciech Trocki <wtrocki@redhat.com>
2019-10-31 23:27:47 +0000

    fix: revert invalid changes commited by mistake

--- templates/apollo-starter-ts/src/resolvers/index.ts
+++ templates/apollo-starter-ts/src/resolvers/index.ts
@@ -1,3 +1,11 @@
-import { testResolvers } from './generated/test'
+export const resolvers = {
+  Query: {
+    me: (obj: any, args: any, context: any, info: any) => {
+      // we can access the request object provided by the Voyager framework
 
-export const resolvers = [testResolvers]
+      // we can access the context added below also
+      // console.log(context.serverName)
+      return { id: 1, name: `GraphBack` };
+    }
+  }
+}
--- templates/apollo-starter-ts/src/schema/generated.ts
+++ templates/apollo-starter-ts/src/schema/generated.ts
@@ -1,31 +1,9 @@
-
 export const schemaString = `
-
-
-
-  type Test {
-    id: ID
-    name: String
-  }
-
-  input TestInput {
-    name: String
-  }
-
-  input TestFilter {
-    id: ID
-    name: String
-  }
-
-
-  type Query {
-    findTests(fields: TestFilter!): [Test!]!
-    findAllTests: [Test!]!
-  }
-
-  type Mutation {
-    createTest(input: TestInput!): Test!
-    updateTest(id: ID!, input: TestInput!): Test!
-  }
-
-`;
+type User {
+  id: ID
+  name: String
+}
+type Query {
+  me: User
+}
+`
