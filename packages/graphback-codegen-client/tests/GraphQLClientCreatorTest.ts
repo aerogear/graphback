54353e93d3f2c61188c4191bfe1f60cb7226477b
Wojciech Trocki <wtrocki@redhat.com>
2019-11-01 16:33:27 +0000

    fix: extra test

--- packages/graphback-codegen-client/tests/GraphQLClientCreatorTest.ts
+++ packages/graphback-codegen-client/tests/GraphQLClientCreatorTest.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line: match-default-export-name no-implicit-dependencies
 import { graphQLInputContext } from '@graphback/codegen-core/src';
-import { applyGeneratorDirectives } from '@graphback/codegen-core/src/graphql/directives';
+import { applyGeneratorDirectives } from '@graphback/codegen-core';
 import ava, { ExecutionContext } from 'ava';
 import { readFileSync } from 'fs';
 import { buildSchema, parse } from 'graphql';
