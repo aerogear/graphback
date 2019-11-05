72ac8ef9d10ce4ce7c4c256c1b94282e97fc7c18
Enda Phelan <ephelan@redhat.com>
2019-11-05 16:53:36 +0000

    test: add GraphQLSchemaManager tests

---
+++ packages/graphback-db-manage/tests/GraphQLSchemaManager.test.ts
@@ -0,0 +1,49 @@
+// tslint:disable-next-line: match-default-export-name no-implicit-dependencies
+import ava, { ExecutionContext } from 'ava';
+import { GraphQLSchemaManager, InMemoryModelProvider } from "../src/database";
+
+const oldSchemaDefault = `
+type User {
+    id: ID!
+    name: String!
+}
+`;
+
+const currentSchemaDefault = `
+type User {
+    id: ID!
+    name: String!
+    age: Int
+}
+
+type Test {
+    id: ID!
+    name: String
+}
+`;
+
+const setup = (oldSchemaText: string = oldSchemaDefault, newSchemaText: string = currentSchemaDefault) => {
+
+  const provider = new InMemoryModelProvider(oldSchemaText, newSchemaText);
+
+  const schemaManager = new GraphQLSchemaManager({ provider: provider });
+
+  return { provider, schemaManager };
+}
+
+
+ava('it should detect multiple changes', (t: ExecutionContext) => {
+  const { schemaManager } = setup();
+
+  const changes = schemaManager.getChanges();
+
+  t.assert(changes.length === 2);
+});
+
+ava('it should update the old schema', (t: ExecutionContext) => {
+  const { schemaManager, provider } = setup();
+
+  schemaManager.updateOldSchema();
+
+  t.assert(provider.getCurrentSchemaText() === provider.getPreviousSchemaText());
+});
