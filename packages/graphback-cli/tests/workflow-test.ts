cd5721c5fa2fed236456279389a441ba62008345
Wojciech Trocki <wtrocki@redhat.com>
2019-11-08 22:53:06 +0000

    fix: update scripts in sample apps

--- packages/graphback-cli/tests/workflow-test.ts
+++ packages/graphback-cli/tests/workflow-test.ts
@@ -35,8 +35,16 @@ ava('Test cli workflow', async (t: ExecutionContext) => {
   t.true(existsSync(join(basePath, "db.sqlite")))
   t.true(existsSync(join(basePath, "client/src/graphql/fragments/Note.ts")))
   t.true(existsSync(join(basePath, "server/src/schema/generated.ts")))
-  t.true(existsSync(join(basePath, "server/src/resolvers/generated/note.ts")))
- 
+  t.true(existsSync(join(basePath, "server/src/resolvers/generated/test.ts")))
+
+  process.chdir(resolve(basePath, './server'))
+  
+  try {
+    await execa('npm', ['run', 'build']);
+  } catch (error) {
+    t.fail(`build failed with ${error}`);
+  }
+
   // Test init config 
   await initConfig("testback ", { model, database: "sqlite3", client: true });
 });
