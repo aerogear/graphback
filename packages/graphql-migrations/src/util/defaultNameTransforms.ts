e13c68c71ab9ab61f67d1fc5e77975d7672d07e5
Enda Phelan <ephelan@redhat.com>
2019-11-29 11:48:45 +0000

    feat: change default name transform

--- packages/graphql-migrations/src/util/defaultNameTransforms.ts
+++ packages/graphql-migrations/src/util/defaultNameTransforms.ts
@@ -1,9 +1,8 @@
-import { snake } from 'case'
 import { NameTransformDirection } from '../abstract/generateAbstractDatabase'
 
 export function defaultNameTransform(name: string, direction: NameTransformDirection) {
   if (direction === 'to-db') {
-    return snake(name)
+    return name.toLowerCase();
   }
   return name
 }
