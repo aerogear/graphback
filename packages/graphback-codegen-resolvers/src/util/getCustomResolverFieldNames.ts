ab8d6ece85bb6eff0638995b59aaa21ea11e8056
Enda Phelan <ephelan@redhat.com>
2020-01-29 14:30:38 +0000

    refactor: pass name instead of full object

--- packages/graphback-codegen-resolvers/src/util/getCustomResolverFieldNames.ts
+++ packages/graphback-codegen-resolvers/src/util/getCustomResolverFieldNames.ts
@@ -1,17 +1,17 @@
 import { getBaseType } from '@graphback/core';
 import { GraphQLField, GraphQLObjectType } from "graphql";
 
-export function getCustomTypeResolverFieldNames(graphqlType: GraphQLObjectType, resolverObject: GraphQLObjectType, generatedResolverKeys: string[]) {
-    const typeResolverFields = getTypeResolvers(graphqlType, resolverObject);
+export function getCustomTypeResolverFieldNames(graphqlTypeName: string, resolverObject: GraphQLObjectType, generatedResolverKeys: string[]) {
+    const typeResolverFields = getTypeResolvers(graphqlTypeName, resolverObject);
     const modelKeys = typeResolverFields.map((field: GraphQLField<any, any>) => field.name);
 
     return findUniqueKeys(generatedResolverKeys, modelKeys);
 }
 
-function getTypeResolvers(graphqlType: GraphQLObjectType, resolverType: GraphQLObjectType) {
+function getTypeResolvers(graphqlTypeName: string, resolverType: GraphQLObjectType) {
     const resolverFields = Object.values(resolverType.getFields());
 
-    return resolverFields.filter((field: GraphQLField<any, any>) => getBaseType(field.type).name === graphqlType.name);
+    return resolverFields.filter((field: GraphQLField<any, any>) => getBaseType(field.type).name === graphqlTypeName.name);
 }
 
 // Finds the keys that are unique to `allKeys`
