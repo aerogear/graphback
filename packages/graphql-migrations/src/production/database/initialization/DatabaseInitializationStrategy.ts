80236cc5eaeba1126ce806f850be27ed2304ec56
Wojciech Trocki <wtrocki@redhat.com>
2020-01-31 09:31:23 +0000

    BREAKING: Remove production migrations

--- packages/graphql-migrations/src/production/database/initialization/DatabaseInitializationStrategy.ts
+++
@@ -1,7 +0,0 @@
-import { InputModelTypeContext } from '@graphback/core';
-import { Change } from '@graphql-inspector/core';
-import { DatabaseContextProvider } from '../migrations/DatabaseContextProvider';
-
-export interface DatabaseInitializationStrategy {
-  init(schemaText: string): Promise<void>;
-}
--- packages/graphql-migrations/src/production/database/migrations/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './DataResourcesManager';
-export * from './DatabaseContextProvider';
