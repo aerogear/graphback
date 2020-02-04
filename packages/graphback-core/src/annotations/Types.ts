cbfb12ef4d1824bf4e829cadd0dc4fbd0f563b67
Enda Phelan <ephelan@redhat.com>
2020-02-04 12:48:13 +0000

    chore: delete unused file

--- packages/graphback-core/src/annotations/Types.ts
+++
@@ -1,7 +0,0 @@
-import { TypeDefinitionNode } from 'graphql';
-import { parseAnnotations } from 'graphql-metadata';
-
-export const parseTypeAnnotations = (node: TypeDefinitionNode) => {
-
-    return node.description ? parseAnnotations('crud', String(node.description)) : undefined;
-}
