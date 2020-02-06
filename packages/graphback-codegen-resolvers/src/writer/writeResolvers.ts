823bed587c83bec7ad0d03406269eda973ad9bde
Wojciech Trocki <wtrocki@redhat.com>
2020-02-06 17:40:25 +0000

    fix: Resolvers formatting for js

--- packages/graphback-codegen-resolvers/src/writer/writeResolvers.ts
+++ packages/graphback-codegen-resolvers/src/writer/writeResolvers.ts
@@ -1,21 +1,10 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
-import * as prettier from 'prettier';
+
 import { OutputResolvers, ResolverOutputDefinition } from '../output/outputResolvers';
 import { ResolverGeneratorPluginConfig } from '../ResolverGeneratorPlugin';
 
-// TODO: Move code formatting 
-// TODO BROKEN for JS. THis should be part of the resolver formatter.
-function formatDocument(contents: string) {
-    try {
-        return prettier.format(contents, { semi: false, parser: 'typescript' });
-    } catch (e) {
-        // tslint:disable-next-line: no-console
-        console.log("Cannot format resolvers implementation", e)
 
-        return contents;
-    }
-}
 
 export function writeResolvers(outputResolvers: OutputResolvers, options: ResolverGeneratorPluginConfig) {
     // TODO (this should be configurable
@@ -25,18 +14,18 @@ export function writeResolvers(outputResolvers: OutputResolvers, options: Resolv
     createFolders(customResolversPath, generatedResolversPath);
 
     outputResolvers.generated.resolvers.forEach((resolverDefinition: ResolverOutputDefinition) => {
-        writeFileSync(`${generatedResolversPath}/${resolverDefinition.name}.${options.format}`, formatDocument(resolverDefinition.output));
+        writeFileSync(`${generatedResolversPath}/${resolverDefinition.name}.${options.format}`, resolverDefinition.output);
     });
-    writeFileSync(`${generatedResolversPath}/index.${options.format}`, formatDocument(outputResolvers.generated.index));
+    writeFileSync(`${generatedResolversPath}/index.${options.format}`, outputResolvers.generated.index);
 
     outputResolvers.custom.resolvers.forEach((resolverDefinition: ResolverOutputDefinition) => {
         const fileName = `${customResolversPath}/${resolverDefinition.name}.${options.format}`;
         if (!existsSync(fileName)) {
-            writeFileSync(fileName, formatDocument(resolverDefinition.output));
+            writeFileSync(fileName, resolverDefinition.output);
         }
     });
-    writeFileSync(`${customResolversPath}/index.${options.format}`, formatDocument(outputResolvers.custom.index));
-    writeFileSync(`${options.outputPath}/index.${options.format}`, formatDocument(outputResolvers.index));
+    writeFileSync(`${customResolversPath}/index.${options.format}`, outputResolvers.custom.index);
+    writeFileSync(`${options.outputPath}/index.${options.format}`, outputResolvers.index);
 }
 
 function createFolders(generatedResolversPath: string, customResolversPath: string) {
