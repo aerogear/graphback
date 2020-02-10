79b327af2d57d75e8e1a34974ca5129331c1ddfa
Enda Phelan <ephelan@redhat.com>
2020-02-10 10:59:49 +0000

    chore: remove unused functions

--- packages/graphback-core/src/db/dataMapper.ts
+++ packages/graphback-core/src/db/dataMapper.ts
@@ -12,45 +12,6 @@ export interface TableID {
     value: any
 }
 
-export const mapDataToTable = (data: any, modelMap: ModelTableMap): ModelDataMap => {
-    const fieldMap = modelMap.fieldMap;
-
-    const dataMap: ModelDataMap = {
-        table: modelMap.tableName,
-        fieldMap
-    }
-
-    if (!fieldMap) {
-        return dataMap;
-    }
-
-    dataMap.data = {};
-    for (const key of Object.keys(data)) {
-        const newKey = fieldMap[key] ? fieldMap[key] : key;
-        if (data[key]) {
-            dataMap.data[newKey] = data[key];
-        }
-    }
-
-    return dataMap;
-}
-
-export const mapDataFromTable = (modelName: string, data: any, modelsMap: ModelTableMap[]): any => {
-    const modelMap = getModelTableMap(modelName, modelsMap);
-    const fieldMap = modelMap.fieldMap;
-
-    const mappedData = {};
-    for (const key of Object.keys(fieldMap)) {
-        if (fieldMap[key] && data[fieldMap[key]]) {
-            const newKey = fieldMap[key];
-            mappedData[key] = data[newKey];
-        }
-    }
-
-    return mappedData;
-}
-
-// TODO: Define interface for data
 export const getUpdateArgs = (idField: string, data: any, fieldMap?: any): ModelDataMap => {
     const mappedData: ModelDataMap = {
         id: {
