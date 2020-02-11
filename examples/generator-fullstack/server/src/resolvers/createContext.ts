4c13b0fd9dbbecb6e11176501cb125954be65a3b
Wojciech Trocki <wtrocki@redhat.com>
2020-02-11 19:21:52 +0000

    fix: default context argument list

--- examples/generator-fullstack/server/src/resolvers/createContext.ts
+++ examples/generator-fullstack/server/src/resolvers/createContext.ts
@@ -1,6 +1,6 @@
 import { GraphQLSchema } from "graphql"
 
-export const createCRUDResolversRuntimeContext = (schema: GraphQLSchema, db: any, pubSub: any) => {
+export const createCRUDResolversRuntimeContext = (input: { schema: GraphQLSchema, db: any, pubSub: any }) => {
 
   return {}
 }
