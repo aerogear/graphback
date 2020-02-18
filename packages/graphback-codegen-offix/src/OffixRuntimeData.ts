11dd82af59a7777778c3d581a394cb92940b402d
Wojciech Trocki <wtrocki@redhat.com>
2020-02-18 10:29:02 +0000

    fix: Minor lint issues

--- packages/graphback-codegen-offix/src/OffixRuntimeData.ts
+++ packages/graphback-codegen-offix/src/OffixRuntimeData.ts
@@ -3,18 +3,26 @@ import { GraphQLObjectType } from 'graphql';
 import { getDatabaseArguments } from '@graphback/core';
 
 
+/**
+ * Data provider for offix that can throw conflict for edits
+ */
 export class OffixDataProvider extends PgKnexDBDataProvider {
     public constructor(baseType: GraphQLObjectType, db: any) {
         super(baseType, db);
     }
 
+    /**
+     * @param data 
+     */
     public async update(data: any): Promise<any> {
         const { idField, data: updateData } = getDatabaseArguments(this.tableMap, data);
         //tslint:disable-next-line: await-promise
         const dbResult = await this.db.select().from(this.tableName).where(idField.name, '=', idField.value);
         if (dbResult && dbResult[0]) {
             if (data.version !== dbResult[0].version) {
-                throw { conflictInfo: { serverState: dbResult, clientState: data } }
+                const conflictError: any = new Error();
+                conflictError.conflictInfo = { serverState: dbResult, clientState: data };
+                throw conflictError
             }
 
             //tslint:disable-next-line: await-promise
@@ -27,4 +35,6 @@ export class OffixDataProvider extends PgKnexDBDataProvider {
         }
         throw new NoDataError(`Cannot update ${this.tableName}`);
     }
+
+    // TODO Support for deletes
 }
