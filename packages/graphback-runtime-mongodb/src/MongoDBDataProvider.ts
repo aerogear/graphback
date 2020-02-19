eb8530b346306af10d20a4c2f1635e4af61bbe36
Wojciech Trocki <wtrocki@redhat.com>
2020-02-19 13:55:09 +0000

    fix: Refactor mongo implementation to work with mapping

--- packages/graphback-runtime-mongodb/src/MongoDBDataProvider.ts
+++ packages/graphback-runtime-mongodb/src/MongoDBDataProvider.ts
@@ -1,76 +1,66 @@
 import { GraphQLObjectType } from 'graphql';
+import { ObjectId, Db } from "mongodb"
 import { ModelTableMap, buildModelTableMap, getDatabaseArguments } from '@graphback/core';
 import { GraphbackDataProvider, NoDataError, AdvancedFilter } from '@graphback/runtime';
-const { ObjectId } = require('mongodb');
-
 
 export class MongoDBDataProvider<Type = any, GraphbackContext = any> implements GraphbackDataProvider<Type, GraphbackContext>{
-  private db: any;
-  protected tableName: string;
+  protected db: Db;
+  protected collectionName: string;
   protected tableMap: ModelTableMap;
 
-  constructor(baseType: GraphQLObjectType, client: any) {
-    client.connect((result: any) => {
-      this.db = client.db("graphql_practice");
-    });
-    //TODO build and use mapping here
+  constructor(baseType: GraphQLObjectType, db: any) {
+    this.db = db;
     this.tableMap = buildModelTableMap(baseType);
-    this.tableName = this.tableMap.tableName;
+    this.collectionName = this.tableMap.tableName;
   }
 
-  public async create(data: any): Promise<Type> {
-    // const { data: createData } = getDatabaseArguments(this.tableMap, data);
-    const result = await this.db.collection(this.tableName).insertOne(data);
-    if (result) {
-      return {
-        ...result.ops[0],
-        id: result.ops[0]._id
-      }
+  public async create(data: Type): Promise<Type> {
+    const { idField } = getDatabaseArguments(this.tableMap, data);
+    const result = await this.db.collection(this.collectionName).insertOne(data);
+    if (result && result.ops) {
+      result.ops[0][idField.name] = result.ops[0]._id;
+      return result.ops[0];
     }
-    throw new NoDataError(`Cannot create ${this.tableName}`);
+    throw new NoDataError(`Cannot create ${this.collectionName}`);
   }
 
   public async update(data: Type): Promise<Type> {
-    const { idField, data: updateData } = getDatabaseArguments(this.tableMap, data);
+    const { idField } = getDatabaseArguments(this.tableMap, data);
     if (idField) {
-      const result = await this.db.collection(this.tableName).updateOne({ _id: ObjectId(idField.value) }, { $set: data });
+      const result = await this.db.collection(this.collectionName).updateOne({ _id: new ObjectId(idField.value) }, { $set: data });
       if (result) {
-        const dbResult = await this.db.collection(this.tableName).find({ _id: ObjectId(idField.value) }).toArray();
-        if (dbResult) {
-          return {
-            ...dbResult[0],
-            id: dbResult[0]._id
-          }
+        const queryResult = await this.db.collection(this.collectionName).find({ _id: new ObjectId(idField.value) }).toArray();
+        if (queryResult && queryResult[0]) {
+          queryResult[0][idField.name] = queryResult[0]._id;
+          return queryResult[0];
         } else {
-          throw new NoDataError(`Cannot update ${this.tableName}`);
+          throw new NoDataError(`Cannot update ${this.collectionName}`);
         }
       }
     }
-    throw new NoDataError(`Cannot update ${this.tableName}`);
+    throw new NoDataError(`Cannot update ${this.collectionName}`);
   }
 
   // tslint:disable-next-line: no-reserved-keywords
   public async delete(data: Type): Promise<Type> {
-    const { idField, data: updateData } = getDatabaseArguments(this.tableMap, data);
+    const { idField } = getDatabaseArguments(this.tableMap, data);
     if (idField) {
-      const dbResult = await this.db.collection(this.tableName).find({ _id: ObjectId(idField.value) }).toArray();
-      if (dbResult) {
-        const result = await this.db.collection(this.tableName).deleteOne({ _id: ObjectId(idField.value) }, { $set: data });
-        if (result) {
-          return {
-            ...dbResult[0],
-            id: dbResult[0]._id,
-          };
+      const queryResult = await this.db.collection(this.collectionName).find({ _id: new ObjectId(idField.value) }).toArray();
+      if (queryResult) {
+        const result = await this.db.collection(this.collectionName).deleteOne({ _id: new ObjectId(idField.value) });
+        if (queryResult && queryResult[0]) {
+          queryResult[0][idField.name] = queryResult[0]._id;
+          return queryResult[0];
         } else {
-          throw new NoDataError(`Cannot update ${this.tableName}`);
+          throw new NoDataError(`Cannot update ${this.collectionName}`);
         }
       }
     }
-    throw new NoDataError(`Cannot update ${this.tableName}`);
+    throw new NoDataError(`Cannot update ${this.collectionName}`);
   }
 
   public async findAll(): Promise<Type[]> {
-    const data = await this.db.collection(this.tableName).find({}).toArray();
+    const data = await this.db.collection(this.collectionName).find({}).toArray();
     if (data) {
       return data.map((one: any) => {
         return {
@@ -79,15 +69,17 @@ export class MongoDBDataProvider<Type = any, GraphbackContext = any> implements 
         }
       });
     }
-    throw new NoDataError(`Cannot find all results for ${this.tableName}`);
+    throw new NoDataError(`Cannot find all results for ${this.collectionName}`);
   }
 
   public async findBy(filter: Type | AdvancedFilter): Promise<Type[]> {
     let dbResult;
-    if (filter.id) {
-      dbResult = await this.db.collection(this.tableName).find({ _id: ObjectId(filter.id) }).toArray();
+    const { idField } = getDatabaseArguments(this.tableMap, filter);
+    // TODO MongoDB should use advanced filter with JSON scalar defined as InputType
+    if (filter[idField.name]) {
+      dbResult = await this.db.collection(this.collectionName).find({ _id: new ObjectId(filter[idField.name]) }).toArray();
     } else {
-      dbResult = await this.db.collection(this.tableName).find({ title: filter.title }).toArray();
+      dbResult = await this.db.collection(this.collectionName).find({ title: filter.title }).toArray();
     }
     if (dbResult) {
       return dbResult.map((one: any) => {
@@ -97,22 +89,14 @@ export class MongoDBDataProvider<Type = any, GraphbackContext = any> implements 
         }
       });
     }
-    throw new NoDataError(`No results for ${this.tableName} query and filter: ${JSON.stringify(filter)}`);
+    throw new NoDataError(`No results for ${this.collectionName} query and filter: ${JSON.stringify(filter)}`);
   }
 
   public async batchRead(relationField: string, ids: string[]): Promise<Type[][]> {
-    // const dbResult = await this.db.select().from(name).whereIn(relationField, ids);
+    // TODO
+    return [];
 
-    // if (dbResult) {
 
-    //   const resultsById = ids.map((id: string) => dbResult.filter((data: any) => {
-    //     return data[relationField].toString() === id.toString();
-    //   }))
-
-    //   return resultsById as [Type[]];
-    // }
-
-    throw new NoDataError(`No results for ${this.tableName} and id: ${JSON.stringify(ids)}`);
   }
 
 }
