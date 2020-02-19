52a485da9a6b8139f87be7757647cd97ac881d1f
Tsvetoslav Dimov <baskski@gmail.com>
2020-02-19 14:57:15 +0000

    Separate dbmigrations as a separate extension from graphback.

--- packages/graphback-cli/src/components/db.ts
+++ packages/graphback-cli/src/components/db.ts
@@ -2,7 +2,8 @@ import * as execa from 'execa'
 import { GlobSync } from 'glob'
 import { loadConfig } from 'graphql-config';
 import { migrateDB, MigrateOptions, removeNonSafeOperationsFilter } from 'graphql-migrations';
-import { extensionName, graphbackConfigExtension } from '../config/extension';
+import { graphbackExtension, graphbackConfigExtension } from '../config/graphbackExtension';
+import { dbmigrationsExtension, dbMigrationConfigExtension } from '../config/dbmigrationsExtension';
 import { logError, logInfo } from '../utils'
 import { loadSchema } from './loadSchema';
 
@@ -20,17 +21,22 @@ export const createDBResources = async (cliFlags: { project?: string }): Promise
   try {
     const config = await loadConfig({
       rootDir: process.cwd(),
-      extensions: [graphbackConfigExtension]
+      extensions: [graphbackConfigExtension, dbMigrationConfigExtension]
     });
     const project = config.getProject(cliFlags.project || 'default');
-    const graphbackConfig = project.extension(extensionName);
+    const graphbackConfig = project.extension(graphbackExtension);
+    const dbMigrationConfig = project.extension(dbmigrationsExtension);
 
     if (!graphbackConfig) {
-      throw new Error(`You should provide a valid '${extensionName}' config to generate schema from data model`);
+      throw new Error(`You should provide a valid '${graphbackExtension}' config to generate schema from data model`);
+    }
+
+    if (!dbMigrationConfig) {
+      throw new Error(`You should provide a valid '${dbmigrationsExtension}' config to migrate schema`);
     }
 
     if (!graphbackConfig.model) {
-      throw new Error(`'${extensionName}' config missing 'model' value that is required`);
+      throw new Error(`'${graphbackExtension}' config missing 'model' value that is required`);
     }
 
     const models = new GlobSync(`${graphbackConfig.model}/*.graphql`);
@@ -40,13 +46,13 @@ export const createDBResources = async (cliFlags: { project?: string }): Promise
       process.exit(0)
     }
 
-    if (!graphbackConfig.dbmigrations) {
-      throw new Error(`'${extensionName}' config missing 'dbmigrations' section that that is required. Please check example application for more information`);
-    }
+    // if (!graphbackConfig.dbmigrations) {
+    //   throw new Error(`'${graphbackExtension}' config missing 'dbmigrations' section that that is required. Please check example application for more information`);
+    // }
 
-    if (graphbackConfig.dbmigrations.client === 'sqlite3') {
-      logInfo(`SQLLite database will need to be recreated from scratch with every migration. Please manually remove/backup existing SQLLite database file before migration`)
-      await execa('touch', [graphbackConfig.dbmigrations.connection.filename])
+    if (dbMigrationConfig.client === 'sqlite3') {
+      logInfo(`SQLLite database will need to be recreated from scratch with every migration. Please manually remove/backup existing SQLLite database file before migration`);
+      await execa('touch', [dbMigrationConfig.connection.filename])
     }
 
     const migrateOptions: MigrateOptions = {
@@ -55,7 +61,7 @@ export const createDBResources = async (cliFlags: { project?: string }): Promise
     };
 
     const schemaText = loadSchema(project.schema.toString());
-    databaseOperations = await migrateDB(graphbackConfig.dbmigrations, schemaText, migrateOptions);
+    databaseOperations = await migrateDB(dbMigrationConfig, schemaText, migrateOptions);
   } catch (err) {
     handleError(err)
   }
--- packages/graphback-cli/tests/workflow-test.ts
+++ packages/graphback-cli/tests/workflow-test.ts
@@ -11,21 +11,21 @@ const model = {
       name: String
     }
 `
-}
+};
 
 test('Test cli workflow', async (t: ExecutionContext) => {
   const basePath = resolve(`${__dirname}/../../../templates/ts-apollo-fullstack`);
-  process.chdir(basePath)
+  process.chdir(basePath);
   await initConfig({ model, database: "sqlite3", client: true, skipInstall: true });
   await generateUsingPlugins({});
 
   await createDB();
 
-  t.true(existsSync(join(basePath, "db.sqlite")))
-  t.true(existsSync(join(basePath, "client/src/graphql/fragments/Note.graphql")))
-  t.true(existsSync(join(basePath, "client/src/graphql/queries/findAllNotes.graphql")))
-  t.true(existsSync(join(basePath, "client/src/graphql/mutations/createNote.graphql")))
-  t.true(existsSync(join(basePath, "src/schema/schema.graphql")))
-  t.true(existsSync(join(basePath, "src/resolvers/resolvers.ts")))
+  t.true(existsSync(join(basePath, "db.sqlite")));
+  t.true(existsSync(join(basePath, "client/src/graphql/fragments/Note.graphql")));
+  t.true(existsSync(join(basePath, "client/src/graphql/queries/findAllNotes.graphql")));
+  t.true(existsSync(join(basePath, "client/src/graphql/mutations/createNote.graphql")));
+  t.true(existsSync(join(basePath, "src/schema/schema.graphql")));
+  t.true(existsSync(join(basePath, "src/resolvers/resolvers.ts")));
 });
 
