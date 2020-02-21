47bc8fd1223ea8c7c6fb53522646ba09ac2e395e
Wojciech Trocki <wtrocki@redhat.com>
2020-02-21 16:11:31 +0000

    fix: prepare release reverted

--- scripts/prepareRelease.sh
+++ scripts/prepareRelease.sh
@@ -3,6 +3,10 @@ echo "Preparing release"
 
 set -e
 
+npm install	
+npm run bootstrap	
+npm run build
+
 # don't run in CI
 if [ ! "$CI" = true ]; then
   lerna publish --skip-git --force-publish=* --skip-npm
