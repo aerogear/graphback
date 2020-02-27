2e3ead27c3c60169e019caa1896d201a596477c0
nvs16 <nvsabhishek.india@gmail.com>
2020-02-28 00:18:17 +0530

    added option for mongodb in graphback config

--- packages/graphback-cli/src/components/config.ts
+++ packages/graphback-cli/src/components/config.ts
@@ -13,11 +13,12 @@ export async function installDependencies(database: string): Promise<void> {
   await execa('npm', ['i'])
   if (database === 'pg') {
     await execa('npm', ['i', '-S', 'pg'])
-  } else if (database === 'sqlite3') {
-    await execa('npm', ['i', '-S', 'sqlite3'])
+    await execa('npm', ['i', '-S', '@graphback/runtime-knex'])
+  } else if (database === 'MongoDB') {
+    await execa('npm', ['i', '-S', 'mongodb'])
+    await execa('npm', ['i', '-S', '@graphback/runtime-mongo'])
   }
   //Using opiniated layer
-  await execa('npm', ['i', '-S', '@graphback/runtime-knex'])
 }
 
 function postSetupMessage(): string {
