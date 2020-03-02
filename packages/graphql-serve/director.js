cf364befb43dad567cd08bd5d096450caa936174
Wojciech Trocki <wtrocki@redhat.com>
2020-03-02 17:22:55 +0000

    fix: remove another files

--- packages/graphql-serve/director.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TestxDirector_1 = require("./dist/src/TestxDirector");
-exports.TestxDirector = TestxDirector_1.TestxDirector;
