04ff56dee0611803fd4307c82ffbf318b7b264c8
Wojciech Trocki <wtrocki@redhat.com>
2020-03-02 17:08:35 +0000

    fix: Remove not needed testx examples

--- packages/graphql-serve/examples/karma/link.js
+++
@@ -1,7 +0,0 @@
-const { TestxServer, TestxController } = require("../../dist/src");
-
-// this is a workaround for a glup issue
-// https://github.com/microsoft/azure-pipelines-tasks/issues/9852
-
-exports.TestxController = TestxController;
-exports.TestxServer = TestxServer;
