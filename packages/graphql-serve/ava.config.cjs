6f2153d7d86c2932fb5519a93339bb20cac19a51
Wojciech Trocki <wtrocki@redhat.com>
2020-03-02 17:25:32 +0000

    fix: Remove more files

--- packages/graphql-serve/ava.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  concurrency: 1,
-  files: ["!examples", "!dist"],
-  extensions: ["ts"],
-  require: ["ts-node/register"],
-  environmentVariables: { TS_NODE_FILES: "true" }
-};
