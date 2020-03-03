f4df28ef851940b518e546a46cb6fc6b009c0f0d
Enda Phelan <ephelan@redhat.com>
2020-03-03 12:50:39 +0000

    feat: prevent table deletion

--- templates/ts-apollo-runtime-backend/src/runtime.ts
+++ templates/ts-apollo-runtime-backend/src/runtime.ts
@@ -1,7 +1,6 @@
-
-import { GraphbackRuntime, ModelDefinition, GraphbackGeneratorConfig } from 'graphback'
+import { GraphbackRuntime } from 'graphback'
 import { createKnexPGCRUDRuntimeServices } from '@graphback/runtime-knex'
-import { migrateDB } from 'graphql-migrations';
+import { migrateDB, MigrateOptions, removeNonSafeOperationsFilter } from 'graphql-migrations';
 import { PubSub } from 'graphql-subscriptions';
 import { createDB, getGraphbackConfig, getMigrateConfig } from './db'
 import { loadSchema } from './loadSchema';
@@ -17,10 +16,15 @@ export const createRuntime = async () => {
   const dbmigrationsConfig = await getMigrateConfig();
   const schemaText = loadSchema(graphbackConfig.model);
 
-  // NOTE: For SQLite db should be always recreated
-  // const ops = await migrateDB(dbmigrationsConfig, schemaText);
+  const migrateOptions: MigrateOptions = {
+    //Do not perform delete operations on tables
+    operationFilter: removeNonSafeOperationsFilter
+  };
 
-  // console.log("Migrated database", ops);
+  // NOTE: For SQLite db should be always recreated
+  const ops = await migrateDB(dbmigrationsConfig, schemaText, migrateOptions);
+
+  console.log("Migrated database", ops);
 
   const pubSub = new PubSub();
   const runtimeEngine = new GraphbackRuntime(schemaText, graphbackConfig);
