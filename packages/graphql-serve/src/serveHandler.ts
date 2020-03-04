48b513de6ca771858989b608d8c306dde40f83ae
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-03-04 21:21:31 +0530

    chore: Refactor serve cli
    This patch does the following:
    - Move schema printing functionality into gqlserve
    - Separate cli options and command handler
    - Delete printSchema

---
+++ packages/graphql-serve/src/serveHandler.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+import { buildGraphbackServer } from "./GraphbackServer";
+import { getGraphbackServerConfig } from "./GraphbackServerConfig";
+import { argv } from "../src/args";
+
+export const serve = async (): Promise<void> => {
+    const graphbackConfigOpts = await getGraphbackServerConfig(argv.model);
+    const server = await buildGraphbackServer(graphbackConfigOpts);
+    console.log("Generated GraphQL Schema:\n\n")
+    console.log(server.getSchema());
+    if (!argv.schemaOnly){
+        if (argv.port) {
+            const portNumber = argv.port;
+            if (!isNaN(portNumber)) {
+                console.log("\nSpecified port number is NaN, terminating...\n");
+            } else {
+                console.log("\nStarting server...\n");
+                server.start(portNumber);
+            }
+        } else {
+            console.log("\nNo port number specified.\nStarting server on random empty port...\n");
+            server.start();
+        }
+    }
+};
