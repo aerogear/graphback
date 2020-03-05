d532473b88dd7c588e92f4836d00ab0e273bfb55
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-03-05 14:50:27 +0530

    feat: Add serve and print-schema commands to graphback-cli

--- packages/graphback-cli/src/index.ts
+++ packages/graphback-cli/src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import * as yargs from 'yargs';
+import { serve, printSchema } from 'graphql-serve';
 
 export * from './components'
 export * from './config/dbmigrationsExtension'
@@ -10,6 +11,8 @@ if (require.main === module) {
   // eslint-disable-next-line no-unused-expressions
   yargs
     .commandDir('commands')
+    .command(serve)
+    .command(printSchema)
     .demandCommand(1)
     .strict()
     .recommendCommands()
