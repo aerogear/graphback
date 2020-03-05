a57aeb95210fb28d9358956915f2480d818fb236
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-03-05 19:10:07 +0530

    refactor: start server with gqlserve

--- packages/graphql-serve/bin/gqlserve.ts
+++ packages/graphql-serve/bin/gqlserve.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+import { serve, printSchema } from "../src";
 // eslint-disable-next-line no-unused-expressions
 const argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
-    .commandDir('../src/commands')
+    .command({...serve, command: "* [options]"})
+    .command(printSchema)
     .demandCommand(1, 'Please specify a command')
     .strict()
     .recommendCommands()
