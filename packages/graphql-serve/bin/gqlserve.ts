13027a411029b8219d3ce3327a18a3f5f5738bd2
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-03-06 00:03:12 +0530

    refactor: Minor cleanup of gqlserve cli

--- packages/graphql-serve/bin/gqlserve.ts
+++ packages/graphql-serve/bin/gqlserve.ts
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-import { serve, printSchema } from "../src";
 // eslint-disable-next-line no-unused-expressions
 const argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
-    .command({...serve, command: "* [options]"})
-    .command(printSchema)
+    .commandDir("../src/commands")
     .demandCommand(1, 'Please specify a command')
-    .example('$0 -m . -p 8080', 'generate schema from data model files in current directory and start GraphQL server on port 8080')
-    .example('$0 print-schema --model=modelDir', 'only display generated schema from data model files in modelDir directory and quit')
     .strict()
     .recommendCommands()
     .help()
