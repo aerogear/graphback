3ffbfdabb5d2f3d3623f37779fd45698a6ba7906
Gianluca <gianlucazuccarelli@gmail.com>
2020-03-13 18:31:01 +0000

    fix: graphback runtime jest migration issues

--- packages/graphback-runtime/tests/LayeredRuntimeResolverCreatorTest.ts
+++ packages/graphback-runtime/tests/LayeredRuntimeResolverCreatorTest.ts
@@ -3,7 +3,9 @@ import { buildSchema } from 'graphql';
 import { GraphbackCoreMetadata } from '@graphback/core';
 import { LayeredRuntimeResolverCreator } from '../src';
 
-const test = _test as TestInterface<{}>;
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+// const test = _test as TestInterface<{}>;
 
 const schema = buildSchema(`
 """
@@ -27,7 +29,7 @@ type User {
 `);
 
 
-test('find Todo by text', async (t: ExecutionContext)  => {
+test('find Todo by text', async ()  => {
   const metadata = new GraphbackCoreMetadata({ crudMethods: {} }, schema)
   const generator = new LayeredRuntimeResolverCreator(metadata.getModelDefinitions(), {});
   expect(generator.generate()).toMatchSnapshot();
