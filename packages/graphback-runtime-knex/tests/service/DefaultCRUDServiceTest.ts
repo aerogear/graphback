11f5c31b05e2b13bfb4da4df40cb7c03bc526716
Gianluca <gianlucazuccarelli@gmail.com>
2020-03-13 18:31:23 +0000

    fix: graphback runtime knex jest migration issues

--- packages/graphback-runtime-knex/tests/service/DefaultCRUDServiceTest.ts
+++ packages/graphback-runtime-knex/tests/service/DefaultCRUDServiceTest.ts
@@ -19,8 +19,6 @@ interface Todo {
   text: string;
 }
 
-const test = _test as TestInterface<Context>;
-
 const schema = buildSchema(`
 """
 @model
@@ -48,9 +46,11 @@ const userModel = schema.getType('User') as GraphQLObjectType
 //tslint:disable-next-line: no-any
 const todoModel = schema.getType('Todos') as GraphQLObjectType
 
+let context: Context;
+
 //Create a new database before each tests so that
 //all tests can run parallel
-test.beforeEach(async t => {
+beforeEach(async () => {
   const db = Knex({
     client: 'sqlite3',
     connection: {
@@ -101,30 +101,30 @@ test.beforeEach(async t => {
   const todoService = new CRUDService(todoModel, todoProvider, publishConfig)
   const userService = new CRUDService(userModel, userProvider, publishConfig);
 
-  t.context = { db, provider: todoProvider, todoService, userService };
+  context = { db, provider: todoProvider, todoService, userService };
 });
 
-test('create Todo', async t => {
-  const todo: Todo = await t.context.todoService.create({
+test('create Todo', async () => {
+  const todo: Todo = await context.todoService.create({
     text: 'create a todo',
   });
 
-  t.assert(todo.id === 4);
-  t.assert(todo.text === 'create a todo');
+  expect(todo.id).toEqual(4);
+  expect(todo.text).toEqual('create a todo');
 });
 
-test('update Todo', async t => {
-  const todo: Todo = await t.context.todoService.update({
+test('update Todo', async () => {
+  const todo: Todo = await context.todoService.update({
     id: '1',
     text: 'my updated first todo',
   });
 
-  t.assert(todo.id === 1);
-  t.assert(todo.text === 'my updated first todo');
+  expect(todo.id).toEqual(1);
+  expect(todo.text).toEqual('my updated first todo');
 });
 
-test('delete Todo', async t => {
-  const data = await t.context.todoService.delete({
+test('delete Todo', async () => {
+  const data = await context.todoService.delete({
     id: '3',
     text: 'my updated first todo',
   });
@@ -132,34 +132,34 @@ test('delete Todo', async t => {
   expect(data.id).toEqual(3);
 });
 
-test('find all Todos', async t => {
-  const todos = await t.context.todoService.findAll();
+test('find all Todos', async () => {
+  const todos = await context.todoService.findAll();
 
-  t.assert(todos.length === 3);
+  expect(todos.length).toEqual(3);
 });
 
-test('find Todo by text', async t => {
-  const todos: Todo[] = await t.context.todoService.findBy({
+test('find Todo by text', async () => {
+  const todos: Todo[] = await context.todoService.findBy({
     text: 'the second todo',
   });
 
-  t.assert(todos.length === 1);
-  t.assert(todos[0].id === 2);
+  expect(todos.length).toEqual(1);
+  expect(todos[0].id).toEqual(2);
 });
 
-test('delete User by custom ID field', async t => {
-  const result = await t.context.userService.delete({
+test('delete User by custom ID field', async () => {
+  const result = await context.userService.delete({
     username: 'johndoe123'
   });
 
-  t.assert(result.username === 'johndoe123')
+  expect(result.username).toEqual('johndoe123')
 });
 
-test('update User by custom ID field', async t => {
-  const user = await t.context.userService.update({
+test('update User by custom ID field', async () => {
+  const user = await context.userService.update({
     username: 'johndoe123',
     name: 'Johnny Doe'
   });
 
-  t.assert(user.name === 'Johnny Doe')
+  expect(user.name).toEqual('Johnny Doe');
 });
