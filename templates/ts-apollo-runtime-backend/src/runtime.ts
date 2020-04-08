6747e73aec47775cb41c78c84225fa91a96a4931
Enda Phelan <ephelan@redhat.com>
2020-04-08 12:54:34 +0100

    chore: runtime template improvements

--- templates/ts-apollo-runtime-backend/src/runtime.ts
+++ templates/ts-apollo-runtime-backend/src/runtime.ts
@@ -2,35 +2,35 @@ import { GraphbackRuntime } from 'graphback'
 import { createKnexPGCRUDRuntimeServices } from '@graphback/runtime-knex'
 import { migrateDB, MigrateOptions, removeNonSafeOperationsFilter } from 'graphql-migrations';
 import { PubSub } from 'graphql-subscriptions';
-import { createDB, getGraphbackConfig, getMigrateConfig } from './db'
-import { loadSchema } from './loadSchema';
-import { buildSchema } from 'graphql';
+import { createDB, getProjectConfig } from './utils'
+import path from 'path';
 
 /**
  * Method used to create runtime schema
- * It will be part of of the integration tests
  */
-export const createRuntime = async () => {
-  const db = await createDB();
-  const graphbackConfig = await getGraphbackConfig();
-  const dbmigrationsConfig = await getMigrateConfig();
-  const schemaText = loadSchema(graphbackConfig.model);
+export const createRuntime = () => {
+  const db = createDB();
+  const projectConfig = getProjectConfig();
+  const graphbackConfig = projectConfig.extension('graphback');
+  const dbMigrationsConfig = projectConfig.extension('dbmigrations');
+  const model = projectConfig.loadSchemaSync(path.resolve(graphbackConfig.model, './*.graphql'));
 
   const migrateOptions: MigrateOptions = {
     //Do not perform delete operations on tables
     operationFilter: removeNonSafeOperationsFilter
   };
 
-  // NOTE: For SQLite db should be always recreated
-  const ops = await migrateDB(dbmigrationsConfig, schemaText, migrateOptions);
-
-  console.log("Migrated database", ops);
-
   const pubSub = new PubSub();
-  const runtimeEngine = new GraphbackRuntime(schemaText, graphbackConfig);
+  const runtimeEngine = new GraphbackRuntime(model, graphbackConfig);
   const models = runtimeEngine.getDataSourceModels();
-  const services = createKnexPGCRUDRuntimeServices(models, buildSchema(schemaText), db, pubSub);
+  const services = createKnexPGCRUDRuntimeServices(models, model, db, pubSub);
   const runtime = runtimeEngine.buildRuntime(services);
+
+  // NOTE: For SQLite db should be always recreated
+  migrateDB(dbMigrationsConfig, runtime.schema, migrateOptions)
+    .then((ops) => {
+      console.log("Migrated database", ops);
+    });
 
   return runtime;
 }
