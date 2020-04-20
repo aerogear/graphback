fe5ff449917ed400dc2a618cfd2cd397de8360d7
Enda Phelan <ephelan@redhat.com>
2020-04-20 11:13:22 +0100

    fix: print schema with directives

---
+++ packages/graphback-core/src/utils/printSchemaWithDirectives.ts
@@ -0,0 +1,8 @@
+import { GraphQLSchema } from 'graphql';
+import { SchemaComposer } from 'graphql-compose';
+
+export function printSchemaWithDirectives(schemaOrSDL: GraphQLSchema | string): string {
+  const schemaComposer = new SchemaComposer(schemaOrSDL)
+
+  return schemaComposer.toSDL({ exclude: ['String', 'ID', 'Boolean', 'Float', 'Int'] });
+}
