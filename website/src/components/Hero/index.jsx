6d0680c96b09fc1d81e6a4cb7ade4ed9741228ec
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-27 15:47:43 +0100

    chore: extract code component

--- website/src/components/Hero/index.jsx
+++ website/src/components/Hero/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import classnames from 'classnames';
-import components from '@theme/MDXComponents';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
@@ -31,17 +30,6 @@ export function Hero({ siteConfig }) {
                 </Link>
             </div>
           </div>
-          {/* <div className="col col--6 text--left">
-            <div style={{  }}>
-              <components.pre style={{ width: '600px', marginBottom: 0 }}>
-                <components.code className="language-graphql" style={{ margin: '0 auto' }}>
-                  {
-                    `${require('!!raw-loader!../../schema/datamodel.gql').default}`
-                  }
-                </components.code>
-              </components.pre>
-            </div>
-          </div> */}
         </div>
       </div>
     </header>
