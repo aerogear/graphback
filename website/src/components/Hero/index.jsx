9cc045af31e731d2e4615a9bd9de8718851b5e1e
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-27 12:48:58 +0100

    feat: add docusaurus v2 components

---
+++ website/src/components/Hero/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import classnames from 'classnames';
+import components from '@theme/MDXComponents';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Link from '@docusaurus/Link';
+import styles from './styles.module.css';
+
+export function Hero({ siteConfig }) {
+  return (
+    <header className={classnames('hero hero--primary', styles.heroBanner, styles.heroGraphback)}>
+      <div className="container">
+        <div className="row">
+          <div className="col col--6 col--offset-3 text--left">
+            <div className={styles.heroImage}>
+              <img src="/img/logo.png" alt="logo" />
+            </div>
+            <p className="hero__title">{siteConfig.tagline}</p>
+            <ul>
+              <li>Out of the Box Node.js GraphQL API based on your model</li>
+              <li>Full control over the generated code or runtime framework</li>
+              <li>Manages complexity of database management</li>
+            </ul>
+            <div className={styles.buttons}>
+              <Link
+                className={classnames(
+                  'button button--primary button--lg button--rounded',
+                  styles.getStarted,
+                )}
+                to={useBaseUrl('docs/doc1')}>
+                Get Started
+                </Link>
+            </div>
+          </div>
+          {/* <div className="col col--6 text--left">
+            <div style={{  }}>
+              <components.pre style={{ width: '600px', marginBottom: 0 }}>
+                <components.code className="language-graphql" style={{ margin: '0 auto' }}>
+                  {
+                    `${require('!!raw-loader!../../schema/datamodel.gql').default}`
+                  }
+                </components.code>
+              </components.pre>
+            </div>
+          </div> */}
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ website/src/components/Video/index.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+function VideoModal({ open, close}) {
+  console.log(open);
+  return (
+    <div className={classnames(styles.modalWindow, { 'hide-modal': !open })}>
+      <div className={styles.modalBackground} />
+      <a href='#video' className={styles.close} onClick={close}>close</a>
+      <div className={styles.modalContent}>
+        <div className={styles.youtubeWrapper}>
+          <iframe 
+            className={styles.youtubeIFrame} 
+            frameBorder="0" 
+            width="560" 
+            height="310" scrolling="no" 
+            marginHeight="0" 
+            marginWidth="0" 
+            src="https://www.youtube.com/embed/wmpEiffqxy8?autoplay=1" 
+            frameBorder="0" 
+            allowFullScreen 
+            align="middle"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Video() {
+  const [open, setOpen] = useState(false);
+
+  const toggleModal = (event) => {
+    event.preventDefault();
+    setOpen(!open);
+  }
+
+  return (
+    <>
+      <VideoModal open={open} close={toggleModal} />
+      <section id="video" className={styles.videoSection}>
+        <div className="text--center">
+          <h2 className={styles.subtitle}>Graphback in 10 minutes</h2>
+          <div className={styles.videoComponent}>
+            <a href="#" className={styles.play} onClick={toggleModal}>
+              {/* Play */}
+              <img src={useBaseUrl('img/play.png')} alt=""/>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
