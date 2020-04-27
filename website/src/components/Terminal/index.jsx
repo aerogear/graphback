9373426d93e6868e552032039eaffa574b10a1a2
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-27 12:48:58 +0100

    feat: add docusaurus v2 components

---
+++ website/src/components/Terminal/index.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect } from 'react';
+import styles from './styles.module.css';
+
+function Underscore() {
+
+  const underscoreFlash = () => {
+    const underscore = document.getElementById('underscore');
+    underscore.classList.toggle('hidden');
+  };
+
+  useEffect(() => {
+    // subscribe
+    const interval = setInterval(underscoreFlash, 400);
+    // unsubscribe
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div
+      id="underscore"
+      className={styles.underscore}
+    >
+      &#95;
+    </div>
+  );
+}
+
+function TerminalText({ phrase }) {
+  let lettercount = 1;
+  let reverse = false;
+  let wait = false;
+
+  const typeEffect = async () => {
+    const text = document.getElementById('text');
+    text.innerHTML = phrase.substring(0, lettercount);
+    const callable = !reverse ? typeOutWord : backspace;
+    await handleReverse();
+    if (!waiting()) callable();
+  }
+
+  const waiting = () => {
+    return (
+      wait
+      || (lettercount === phrase.length + 1 && !reverse) 
+      || (lettercount === 0 && reverse)
+    );
+  }
+
+  const typeOutWord = () => {
+    if (lettercount < phrase.length + 1) lettercount++;
+  };
+
+  const backspace = () => {
+    if (lettercount > 0) lettercount--;
+  };
+
+  const handleReverse = async () => {
+    if (!reverse && lettercount === phrase.length + 1) await toggleReverse();
+    if (reverse && lettercount === 0) await toggleReverse();
+  };
+
+  const toggleReverse = () => {
+    reverse = !reverse;
+    wait = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        wait = false;
+        resolve();
+      }, 3000);
+    });
+  };
+
+  useEffect(() => {
+    // subscribe
+    const interval = setInterval(typeEffect, 120);
+    // unsubscribe
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <span id="text"></span>
+  );
+}
+
+export function Terminal() {
+
+  return (
+    <div>
+      <div className={styles.terminalHeader}>
+          <p>Quick start!</p>
+      </div>
+      <div className={styles.terminal}>
+        <span>>&nbsp;&nbsp;</span>
+        <TerminalText phrase={'npx graphback init example'} />
+        <Underscore />
+      </div>
+    </div>
+  );
+}
---
+++ website/src/pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+import { Hero } from '../components/Hero';
+import { Features } from '../components/Features';
+import { Terminal } from '../components/Terminal';
+import { Video } from '../components/Video';
+import { BlogCards } from '../components/BlogCards';
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={siteConfig.title}
+      description="Graphback <head />">
+      <Hero siteConfig={siteConfig} />
+      <Features />
+      <Terminal />
+      <Video />
+      <BlogCards /> 
+    </Layout>
+  );
+}
+
+export default Home;
