fe4b68968909945de8bf2f73d0d4eb0953d98cdd
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-27 12:47:03 +0100

    chore: migrate to docusaurus v2

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -129,7 +129,7 @@ module.exports = {
         docs: {
           path: '../docs',
           routeBasePath: 'docs',
-          sidebarPath: require.resolve('./sidebars.json'),
+          sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/aerogear/graphback/edit/master/website/',
         },
