5bd544f714b6820de638aa41ca7ff77822ae1996
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-28 10:13:44 +0100

    feat: add hero animation

--- website/src/components/Hero/index.jsx
+++ website/src/components/Hero/index.jsx
@@ -10,24 +10,26 @@ export function Hero({ siteConfig }) {
       <div className="container">
         <div className="row">
           <div className="col col--6 col--offset-3 text--left">
-            <div className={styles.heroImage}>
-              <img src="/img/logo.png" alt="logo" />
-            </div>
-            <p className="hero__title">{siteConfig.tagline}</p>
-            <ul>
-              <li>Out of the Box Node.js GraphQL API based on your model</li>
-              <li>Full control over the generated code or runtime framework</li>
-              <li>Manages complexity of database management</li>
-            </ul>
-            <div className={styles.buttons}>
-              <Link
-                className={classnames(
-                  'button button--primary button--lg button--rounded',
-                  styles.getStarted,
-                )}
-                to={useBaseUrl('docs/gettingstarted')}>
-                Get Started
+            <div className={styles.fadeIn}>
+              <div className={styles.heroImage}>
+                <img src="/img/logo.png" alt="logo" />
+              </div>
+              <p className="hero__title">{siteConfig.tagline}</p>
+              <ul>
+                <li>Out of the Box Node.js GraphQL API based on your model</li>
+                <li>Full control over the generated code or runtime framework</li>
+                <li>Manages complexity of database management</li>
+              </ul>
+              <div className={styles.buttons}>
+                <Link
+                  className={classnames(
+                    'button button--primary button--lg button--rounded',
+                    styles.getStarted,
+                  )}
+                  to={useBaseUrl('docs/gettingstarted')}>
+                  Get Started
                 </Link>
+              </div>
             </div>
           </div>
         </div>
