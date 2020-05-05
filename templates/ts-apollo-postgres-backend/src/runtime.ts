08a7e0d470c5ff557e98dee91f911831008c22b0
Wojciech Trocki <wtrocki@redhat.com>
2020-05-05 23:36:22 +0100

    fix: Cleanup templates

--- templates/ts-apollo-postgres-backend/src/runtime.ts
+++ templates/ts-apollo-postgres-backend/src/runtime.ts
@@ -36,7 +36,17 @@ export const createRuntime = () => {
   const runtime = runtimeEngine.buildRuntime(services);
 
   // NOTE: For SQLite db should be always recreated
-  migrateDB(dbMigrationsConfig, runtime.schema, migrateOptions)
+  // TODO
+  migrateDB({
+    client: "pg",
+    "connection": {
+      user: "postgresql",
+      password: "postgres",
+      database: "users",
+      host: "localhost",
+      port: 55432
+    }
+  }, runtime.schema, migrateOptions)
     .then((ops) => {
       console.log("Migrated database", ops);
     });
