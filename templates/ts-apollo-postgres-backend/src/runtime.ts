eafd0ec5f979dd29b53043f869a78472d2d4d3c6
Wojciech Trocki <wtrocki@redhat.com>
2020-05-06 15:23:33 +0100

    fix: Revert runtime changes

--- templates/ts-apollo-postgres-backend/src/runtime.ts
+++ templates/ts-apollo-postgres-backend/src/runtime.ts
@@ -1,45 +1,38 @@
-import { graphback } from 'graphback'
-import { CRUDService as service } from '@graphback/runtime'
-import { PgKnexDBDataProvider as provider } from '@graphback/runtime-knex'
+import { GraphbackRuntime } from 'graphback'
+import { createKnexPGCRUDRuntimeServices } from "@graphback/runtime-knex"
 import { migrateDB, MigrateOptions, removeNonSafeOperationsFilter } from 'graphql-migrations';
 import { PubSub } from 'graphql-subscriptions';
-import { connectDB } from './db'
+import { connectDB, getConnectionDetails } from './db'
 import { loadConfigSync } from 'graphql-config';
-
+import path from 'path';
 /**
  * Method used to create runtime schema
  */
 export const createRuntime = () => {
   const db = connectDB();
-  const pubSub = new PubSub();
 
 
   const migrateOptions: MigrateOptions = {
-    //Do not perform delete operations on tables
+    // Do not perform delete operations on tables
     operationFilter: removeNonSafeOperationsFilter
   };
 
-  const graphbackOptions = {
-    db,
-    pubSub,
-    service,
-    provider,
-  }
+  const projectConfig = loadConfigSync({
+    extensions: [
+      () => ({ name: 'graphback' }),
+    ]
+  }).getDefault()
 
-  const runtime = graphback(graphbackOptions)
+  const graphbackConfig = projectConfig.extension('graphback');
+  const model = projectConfig.loadSchemaSync(path.resolve(graphbackConfig.model));
 
-  // NOTE: For SQLite db should be always recreated
-  // TODO
-  migrateDB({
-    client: "pg",
-    "connection": {
-      user: "postgresql",
-      password: "postgres",
-      database: "users",
-      host: "localhost",
-      port: 55432
-    }
-  }, runtime.schema, migrateOptions)
+  const pubSub = new PubSub();
+  const runtimeEngine = new GraphbackRuntime(model, graphbackConfig);
+  const models = runtimeEngine.getDataSourceModels();
+  const services = createKnexPGCRUDRuntimeServices(models, model, db, pubSub);
+  const runtime = runtimeEngine.buildRuntime(services);
+
+  migrateDB(getConnectionDetails(), runtime.schema, migrateOptions)
     .then((ops) => {
       console.log("Migrated database", ops);
     });
