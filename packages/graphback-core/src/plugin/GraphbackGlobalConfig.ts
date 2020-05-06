d54f0b435bb4908ddd4a8aefb1159ed48cb06e29
Wojciech Trocki <wtrocki@redhat.com>
2020-05-06 17:18:43 +0100

    fix: Unit tests for GraphbackGlobalConfig

--- packages/graphback-core/src/plugin/GraphbackGlobalConfig.ts
+++ packages/graphback-core/src/plugin/GraphbackGlobalConfig.ts
@@ -4,6 +4,5 @@ import { GraphbackCRUDGeneratorConfig } from "./GraphbackCRUDGeneratorConfig";
  * Configuration for crud generators
  */
 export interface GraphbackGlobalConfig {
-  //TODO remove this CRUD config
   crudMethods: GraphbackCRUDGeneratorConfig
 }
