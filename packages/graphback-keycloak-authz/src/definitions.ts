848278638f8b371bc98017dcf8cbd5f07c53645c
Dara Hayes <dara.s.hayes@gmail.com>
2020-05-13 11:31:06 +0100

    fix: update definitions

--- packages/graphback-keycloak-authz/src/definitions.ts
+++ packages/graphback-keycloak-authz/src/definitions.ts
@@ -1,29 +1,14 @@
-export type AuthStrategy = 'role';
-
-export type ModelOperation = 'create' | 'update' | 'delete' | 'read';
-
-export interface ModelAuthRule {
-  allow: AuthStrategy,
-  operations?: ModelOperation[],
-  roles: string[],
-};
-
-export interface ModelAuthConfig {
-  name: string
-  auth: {
-    rules: ModelAuthRule[]
-  }
-};
-
-export type ModelAuthConfigs = ModelAuthConfig[];
-
 export type CrudOperationAuthConfig = {
   roles: string[]
 };
 
-export type CrudServiceAuthConfig= {
+export type CrudServiceAuthConfig = {
   create: CrudOperationAuthConfig,
   read: CrudOperationAuthConfig,
   update: CrudOperationAuthConfig,
   delete: CrudOperationAuthConfig,
 };
+
+export type CrudServicesAuthConfig = {
+  [key: string]: CrudServiceAuthConfig
+}
