6a7463b9db9c6ef1ec75f878983f6232debd03a2
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-05-15 10:48:27 +0530

    feat: Add experimental support for foreign key and custom indexing

--- packages/graphback-runtime-mongodb/src/MongoDBDataProvider.ts
+++ packages/graphback-runtime-mongodb/src/MongoDBDataProvider.ts
@@ -1,7 +1,8 @@
 import { GraphQLObjectType } from 'graphql';
 import { ObjectId, Db, Cursor } from "mongodb"
-import { ModelTableMap, buildModelTableMap, getDatabaseArguments } from '@graphback/core';
 import { GraphbackDataProvider, GraphbackPage, NoDataError, GraphbackOrderBy, getFieldTransformations, FieldTransform, TransformType, FieldTransformMap } from '@graphback/runtime';
+import { ModelTableMap, buildModelTableMap, getDatabaseArguments, parseRelationshipAnnotation } from '@graphback/core';
+import { parseAnnotations } from "graphql-metadata";
 import { buildQuery } from './queryBuilder'
 
 interface SortOrder {
@@ -22,10 +23,39 @@ export class MongoDBDataProvider<Type = any, GraphbackContext = any> implements 
     this.tableMap = buildModelTableMap(baseType);
     this.collectionName = this.tableMap.tableName;
     this.fieldTransformMap = getFieldTransformations(baseType);
+
+    Object.keys(baseType.getFields()).forEach((k: string) => {
+      const relationshipData = parseRelationshipAnnotation(baseType.getFields()[k].description)
+      if (relationshipData?.kind && ['manyToOne', 'manyToMany'].includes(relationshipData.kind)) {
+        this.db.collection(this.collectionName).createIndex({
+          [relationshipData.key]: 1
+        }, (e:any, r:any) => {
+          if (e) {
+            console.error(e);
+          } else {
+            console.log(JSON.stringify(r,undefined,4))
+          }
+        });
+      }
+
+      const otherAnnotations = parseAnnotations('db', baseType.getFields()[k].description) as any;
+      if (otherAnnotations?.index) {
+        this.db.collection(this.collectionName).createIndex({
+          [baseType.getFields()[k].name]: 1
+        }, (e: any, r: any) => {
+          if (e) {
+            console.error(e);
+          } else {
+            console.log(JSON.stringify(r,undefined,4))
+          }
+        });
+      }
+    })
   }
   
   public setBaseType(baseType: GraphQLObjectType<any, any, { [key: string]: any; }>): void {
     throw new Error("Method not implemented.");
+
   }
 
   public async create(data: any): Promise<Type> {
