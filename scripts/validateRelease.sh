02b3fafdedaaf75a5475aa1e9e90b407d9a5f69e
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-05-25 16:50:39 +0200

    chore: use locally installed lerna instead of a global one

--- scripts/validateRelease.sh
+++ scripts/validateRelease.sh
@@ -3,12 +3,11 @@
 # explicit declaration that this script needs a $TAG variable passed in e.g TAG=1.2.3 ./script.sh
 TAG=$TAG
 TAG_SYNTAX='^[0-9]+\.[0-9]+\.[0-9]+(-.+)*$'
-
 # get version found in lerna.json. This is the source of truth
 PACKAGE_VERSION=$(cat lerna.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
 # get names of packages being managed by lerna
-PACKAGES=$(lerna --loglevel=silent ls | awk -F ' ' '{print $1}')
+PACKAGES=$(yarn lerna --loglevel=silent ls | awk -F ' ' '{print $1}')
 
 # validate tag has format x.y.z
 if [[ "$(echo $TAG | grep -E $TAG_SYNTAX)" == "" ]]; then
@@ -24,7 +23,7 @@ fi
 
 # validate that all packages have the same version found in lerna.json
 for package in $PACKAGES; do
-  version=$(lerna --loglevel=silent ls -l | grep $package | awk -F ' ' '{print $2}' | cut -c2-)
+  version=$(yarn lerna --loglevel=silent ls -l | grep $package | awk -F ' ' '{print $2}' | cut -c2-)
   if [[ $version =~ $PACKAGE_VERSION ]]; then
     echo "package $package has version $version"
   else
