d2b547b63e1ec7d6bf5ad5de31ea56f427df55ac
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-06-15 23:12:16 +0530

    test(datasync): update snapshots

--- packages/graphback-datasync/tests/DataSyncMongoDBTests.ts
+++ packages/graphback-datasync/tests/DataSyncMongoDBTests.ts
@@ -136,7 +136,7 @@ describe('Soft deletion test', () => {
       }
     });
 
-    expect(deltaPosts.length).toEqual(2));
+    expect(deltaPosts.length).toEqual(2);
 
     for (const p of deltaPosts) {
       expect(p.text).toMatch(/^post/g);
