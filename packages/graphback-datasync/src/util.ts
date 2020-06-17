94d69c9e2d0c13a36325fbf6a2f29362481cc642
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-06-17 20:48:02 +0530

    fix(datasync): implement type safe sync resolver creation

--- packages/graphback-datasync/src/util.ts
+++ packages/graphback-datasync/src/util.ts
@@ -1,6 +1,15 @@
-import { ModelDefinition } from '@graphback/core';
-import { parseMetadata } from 'graphql-metadata'
+import { ModelDefinition, GraphbackCRUDService } from '@graphback/core';
+import { parseMetadata } from 'graphql-metadata';
+import { DataSyncCRUDService } from "./services";
 
 export function isDataSyncModel(model: ModelDefinition): boolean {
   return parseMetadata("delta", model.graphqlType) && parseMetadata("versioned", model.graphqlType)
+}
+
+export function getDataSyncService(service: GraphbackCRUDService): DataSyncCRUDService {
+  if (service instanceof DataSyncCRUDService) {
+    return service;
+  }
+
+  throw Error("Service is not a DataSyncCRUDService. Please use DataSyncCRUDService and DataSync-compliant DataProvider with DataSync Plugin to get Delta Queries.")
 }
