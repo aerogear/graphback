6d0770a437ee2bdefe1b68bedf04e06cda7044a7
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-06-18 22:29:49 +0530

    feat: restore basic conflicts in datasync

--- packages/graphback-datasync/src/index.ts
+++ packages/graphback-datasync/src/index.ts
@@ -1,6 +1,7 @@
 export * from './DataSyncPlugin'
 export * from './providers';
 export * from './services';
+export * from './util';
 
 //Required for plugins
 export { DataSyncPlugin as Plugin } from './DataSyncPlugin'
--- packages/graphback-datasync/tests/DataSyncMongoDBTests.ts
+++ packages/graphback-datasync/tests/DataSyncMongoDBTests.ts
@@ -1,63 +1,65 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { ObjectID, MongoClient } from 'mongodb';
 import { buildSchema } from 'graphql';
-import { filterModelTypes, GraphbackCoreMetadata } from '@graphback/core';
+import { filterModelTypes, GraphbackCoreMetadata, metadataMap } from '@graphback/core';
 import { advanceTo, advanceBy } from "jest-date-mock";
 import { SchemaCRUDPlugin } from "@graphback/codegen-schema";
-import { DataSyncMongoDBDataProvider, DataSyncPlugin } from '../src';
+import { NoDataError } from "@graphback/runtime-mongo";
+import { DataSyncMongoDBDataProvider, DataSyncPlugin, ConflictError } from '../src';
 
+const {fieldNames} = metadataMap;
 export interface Context {
-    providers: { [modelname: string]: DataSyncMongoDBDataProvider };
-    server: MongoMemoryServer
+  providers: { [modelname: string]: DataSyncMongoDBDataProvider };
+  server: MongoMemoryServer
+}
+
+export async function createTestingContext(schemaStr: string, config?: { seedData: { [collection: string]: any[] } }): Promise<Context> {
+  // Setup graphback
+  let schema = buildSchema(schemaStr);
+
+  const server = new MongoMemoryServer();
+  const client = new MongoClient(await server.getConnectionString(), { useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db('test');
+
+  const defautConfig = {
+    "create": true,
+    "update": true,
+    "findOne": true,
+    "find": true,
+    "delete": true,
+    "subCreate": true,
+    "subUpdate": true,
+    "subDelete": true
   }
 
-  export async function createTestingContext(schemaStr: string, config?: { seedData: { [collection: string]: any[] } }): Promise<Context> {
-      // Setup graphback
-      let schema = buildSchema(schemaStr);
+  const schemaGenerator = new SchemaCRUDPlugin({ outputPath: './tmp', format: 'graphql' })
+  const DataSyncGenerator = new DataSyncPlugin()
+  const metadata = new GraphbackCoreMetadata({
+    crudMethods: defautConfig
+  }, schema)
+  metadata.setSchema(schemaGenerator.transformSchema(metadata))
+  metadata.setSchema(DataSyncGenerator.transformSchema(metadata));
+  schema = metadata.getSchema();
 
-      const server = new MongoMemoryServer();
-      const client = new MongoClient(await server.getConnectionString(), { useUnifiedTopology: true });
-      await client.connect();
-      const db = client.db('test');
+  const providers: { [name: string]: DataSyncMongoDBDataProvider } = {}
+  const models = filterModelTypes(schema)
+  for (const modelType of models) {
+    providers[modelType.name] = new DataSyncMongoDBDataProvider(modelType, db);
+  }
 
-      const defautConfig = {
-        "create": true,
-        "update": true,
-        "findOne": true,
-        "find": true,
-        "delete": true,
-        "subCreate": true,
-        "subUpdate": true,
-        "subDelete": true
+  // if seed data is supplied, insert it into collections
+  if (config?.seedData) {
+    const collectionNames = Object.keys(config.seedData);
+    for (const collectionName of collectionNames) {
+      for (const element of config.seedData[collectionName]) {
+        await providers[collectionName].create(element);
       }
+    };
+  }
 
-      const schemaGenerator = new SchemaCRUDPlugin({ outputPath: './tmp', format: 'graphql'})
-      const DataSyncGenerator = new DataSyncPlugin()
-      const metadata = new GraphbackCoreMetadata({
-        crudMethods: defautConfig
-      }, schema)
-      metadata.setSchema(schemaGenerator.transformSchema(metadata))
-      metadata.setSchema(DataSyncGenerator.transformSchema(metadata));
-      schema = metadata.getSchema();
-
-      const providers: { [name: string]: DataSyncMongoDBDataProvider } = {}
-      const models = filterModelTypes(schema)
-      for (const modelType of models) {
-        providers[modelType.name] = new DataSyncMongoDBDataProvider(modelType, db);
-      }
-
-      // if seed data is supplied, insert it into collections
-      if (config?.seedData) {
-        const collectionNames = Object.keys(config.seedData);
-        for (const collectionName of collectionNames) {
-          for (const element of config.seedData[collectionName]) {
-            await providers[collectionName].create(element);
-          }
-        };
-      }
-
-      return { server, providers }
-    }
+  return { server, providers }
+}
 
 describe('Soft deletion test', () => {
   let context: Context;
@@ -76,7 +78,7 @@ describe('Soft deletion test', () => {
     }
     `);
 
-    const { Post }  = context.providers;
+    const { Post } = context.providers;
 
     const first = await Post.create({ text: 'TestPost' });
     expect(first._deleted).toEqual(false)
@@ -95,18 +97,19 @@ describe('Soft deletion test', () => {
     }
     `);
 
-    const { Post }  = context.providers;
+    const { Post } = context.providers;
     const startTime = 1590679886048;
     const deleteTime = 1590679887032;
     advanceTo(startTime);
     const { id } = await Post.create({ text: 'TestPost' });
     advanceTo(deleteTime);
-    const deletedPost = await Post.delete({ id });
+    const deletedPost = await Post.delete({ id, [fieldNames.updatedAt]: startTime });
     expect(deletedPost._deleted).toEqual(true);
     expect(deletedPost.updatedAt).toEqual(deleteTime);
 
     // Tests that we cannot find it anymore
-    expect(Post.findOne({id})).rejects.toThrow();
+    // This test fails if we can still find the document
+    await expect(Post.findOne({ id })).rejects.toThrowError(NoDataError);
   })
 
   it('can filter sync queries', async () => {
@@ -122,7 +125,7 @@ describe('Soft deletion test', () => {
     }
     `);
 
-    const { Post }  = context.providers;
+    const { Post } = context.providers;
 
     // Create some posts
     for (const postTitle of ["post", "post2", "trains"]) {
@@ -142,5 +145,35 @@ describe('Soft deletion test', () => {
       expect(p.text).toMatch(/^post/g);
     }
 
+  })
+
+  it('throws conflict error for update/delete conflicts', async () => {
+    context = await createTestingContext(`
+    """
+    @model
+    @versioned
+    @delta
+    """
+    type Post {
+      id: ID!
+      text: String!
+    }
+    `);
+
+    const { Post } = context.providers;
+    const startTime = 1590679886048;
+    const updateTime = 1590679887032;
+    advanceTo(startTime);
+    const { id } = await Post.create({ text: 'TestPost' });
+    
+    // Update document once
+    advanceTo(updateTime);
+    await Post.update({ id, text: 'updated post text', updatedAt: startTime });
+
+    // Try to update document again with older timestamp
+    await expect(Post.update({ id, text: 'updated post text 2', updatedAt: startTime })).rejects.toThrowError(ConflictError);
+
+    // Try to deletee document with older timestamp
+    await expect(Post.delete({ id, updatedAt: startTime })).rejects.toThrowError(ConflictError);
   })
 })
