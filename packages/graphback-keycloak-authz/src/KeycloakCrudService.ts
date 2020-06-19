02fb860a2a1db6d63e9fdb2dc659422180420964
Wojciech Trocki <wtrocki@redhat.com>
2020-06-19 16:14:48 +0100

    fix: wrapper for keycloak service

--- packages/graphback-keycloak-authz/src/KeycloakCrudService.ts
+++ packages/graphback-keycloak-authz/src/KeycloakCrudService.ts
@@ -1,14 +1,10 @@
 import {
-  CRUDService,
-  GraphbackDataProvider,
   GraphbackPage,
   ResultList,
   GraphbackOrderBy
 } from "@graphback/runtime";
-import { GraphQLObjectType } from 'graphql';
 import { isAuthorizedByRole } from "keycloak-connect-graphql";
-import { GraphbackCRUDGeneratorConfig } from '@graphback/core';
-import { PubSubEngine } from 'graphql-subscriptions';
+import { GraphbackCRUDService } from '../../graphback-core/src/runtime/GraphbackCRUDService';
 import { CrudServiceAuthConfig } from './definitions';
 import { getEmptyServiceConfig, UnauthorizedError } from "./utils";
 
@@ -16,11 +12,15 @@ import { getEmptyServiceConfig, UnauthorizedError } from "./utils";
  * options object for the KeycloakCrudService
  */
 export type KeycloakCrudServiceOptions = {
-  modelType: GraphQLObjectType,
-  db: GraphbackDataProvider,
-  pubSub: PubSubEngine
-  crudOptions: GraphbackCRUDGeneratorConfig,
+  /**
+   * Configuration for authentication
+   */
   authConfig: CrudServiceAuthConfig,
+
+  /**
+   * Service that will be called by Auth service
+   */
+  service: GraphbackCRUDService
 };
 
 /**
@@ -29,13 +29,14 @@ export type KeycloakCrudServiceOptions = {
  * This is actually quite nice and clean but it does not allow for field level auth.
  * It's still a possibility that we could go with though!
  */
-export class KeycloakCrudService<T = any> extends CRUDService<T> {
+export class KeycloakCrudService<T = any> implements GraphbackCRUDService<T> {
 
   private authConfig: CrudServiceAuthConfig;
+  private wrappingService: GraphbackCRUDService;
 
-  public constructor({ modelType, db, crudOptions, authConfig, pubSub }: KeycloakCrudServiceOptions) {
-    super(modelType.name, db, { pubSub, crudOptions });
+  public constructor({ service, authConfig }: KeycloakCrudServiceOptions) {
     this.authConfig = authConfig || getEmptyServiceConfig();
+    this.wrappingService = service;
   }
 
   public create(data: T, context: any): Promise<T> {
@@ -46,7 +47,7 @@ export class KeycloakCrudService<T = any> extends CRUDService<T> {
       }
     }
 
-    return super.create(data, context);
+    return this.wrappingService.create(data, context);
   }
 
   public update(data: T, context: any): Promise<T> {
@@ -57,7 +58,7 @@ export class KeycloakCrudService<T = any> extends CRUDService<T> {
       }
     }
 
-    return super.update(data, context);
+    return this.wrappingService.update(data, context);
   }
 
   public delete(data: T, context: any): Promise<T> {
@@ -68,7 +69,7 @@ export class KeycloakCrudService<T = any> extends CRUDService<T> {
       }
     }
 
-    return super.delete(data, context);
+    return this.wrappingService.delete(data, context);
   }
 
   public findOne(args: any, context: any): Promise<T> {
@@ -79,7 +80,7 @@ export class KeycloakCrudService<T = any> extends CRUDService<T> {
       }
     }
 
-    return super.findOne(args, context);
+    return this.wrappingService.findOne(args, context);
   }
 
   public findBy(filter: any, context: any, orderBy?: GraphbackOrderBy, page?: GraphbackPage): Promise<ResultList<T>> {
@@ -90,6 +91,22 @@ export class KeycloakCrudService<T = any> extends CRUDService<T> {
       }
     }
 
-    return super.findBy(filter, context, orderBy, page);
+    return this.wrappingService.findBy(filter, orderBy, page, context);
+  }
+
+  public subscribeToCreate(filter?: any, context?: any): AsyncIterator<T> {
+    return this.wrappingService.subscribeToCreate(filter, context)
+  }
+
+  public subscribeToUpdate(filter?: any, context?: any): AsyncIterator<T> {
+    return this.wrappingService.subscribeToUpdate(filter, context)
+  }
+
+  public subscribeToDelete(filter?: any, context?: any): AsyncIterator<T> {
+    return this.wrappingService.subscribeToDelete(filter, context)
+  }
+
+  public batchLoadData(relationField: string, id: string | number, filter: any, context: any) {
+    return this.wrappingService.batchLoadData(relationField, id, filter, context)
   }
 }
