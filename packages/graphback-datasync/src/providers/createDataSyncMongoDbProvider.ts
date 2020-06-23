c9a78e25bd1e6a33f8b47e2fc6407ad1f701d551
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-06-23 11:06:03 +0530

    feat(datasync): add ttl

--- packages/graphback-datasync/src/providers/createDataSyncMongoDbProvider.ts
+++ packages/graphback-datasync/src/providers/createDataSyncMongoDbProvider.ts
@@ -1,6 +1,7 @@
 import { Db } from 'mongodb';
-import { ModelDefinition } from '@graphback/core';
+import { ModelDefinition, defaultTableNameTransform } from '@graphback/core';
 import { MongoDBDataProvider, GraphbackDataProvider } from "@graphback/runtime-mongo"
+import * as Agenda from "agenda";
 import { isDataSyncModel } from '../util';
 import { DataSyncMongoDBDataProvider } from './DatasyncMongoDBDataProvider';
 
@@ -9,9 +10,46 @@ import { DataSyncMongoDBDataProvider } from './DatasyncMongoDBDataProvider';
  *
  * @param {Db} db - MongoDb connection
  */
-export function createDataSyncMongoDbProvider(db: Db): (...args: any[]) => GraphbackDataProvider {
+export function createDataSyncMongoDbProvider(db: Db, TTL:number = -1, jobConfig: Agenda.AgendaConfiguration = {}): (...args: any[]) => GraphbackDataProvider {
+  if (jobConfig?.db === undefined && jobConfig?.mongo === undefined) {
+    jobConfig.mongo = db
+  }
+  const agenda = new Agenda(jobConfig);
+  agenda.start().catch((e: any) => {
+    console.error(`Error occurred while starting job service: ${e}`)
+  })
+  
   return (model: ModelDefinition): GraphbackDataProvider => {
     if (isDataSyncModel(model)) {
+      if (TTL >= 0) {
+        const modelName = model.graphqlType.name;
+        const collectionName = defaultTableNameTransform(modelName, 'to-db');
+        const pruneJobName = `prune:${collectionName}`;
+
+        // Define prune job processor for a collection
+        agenda.define(pruneJobName, async (_: any, done: (e: any) => void) => {
+          db.collection(collectionName).deleteMany({
+            _deleted: true,
+            updatedAt: {
+              $lte: Date.now() - (TTL * 1000)
+            }
+          })
+            .then(() => {
+              done(undefined);
+            })
+            .catch((e: any) => {
+              console.error('prune job failed: ', JSON.stringify(e, undefined, 4));
+              done(e);
+            })
+        });
+
+        // Run prune job every 10 seconds
+        agenda.every('10 seconds', pruneJobName).catch((e:any) => {
+          console.error(`Error: prune job for ${modelName}`)
+        })
+
+      }
+
       return new DataSyncMongoDBDataProvider(model.graphqlType, db)
     } else {
       return new MongoDBDataProvider(model.graphqlType, db)
