400daf5e87e008a3fe336cfbc9b547b6e2437147
Enda Phelan <ephelan@redhat.com>
2020-06-24 14:29:39 +0100

    refactor: don't export function

--- packages/graphback-cli/src/index.ts
+++ packages/graphback-cli/src/index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import * as yargs from 'yargs';
-export * from './config/graphbackExtension'
 
 if (require.main === module) {
   // eslint-disable-next-line no-unused-expressions
