881867641806844f4591c656f5fd36e21657adc2
Enda Phelan <ephelan@redhat.com>
2020-06-25 15:41:31 +0100

    feat: create JSON column fro list with JSON annotation

--- packages/graphql-migrations/src/abstract/generateAbstractDatabase.ts
+++ packages/graphql-migrations/src/abstract/generateAbstractDatabase.ts
@@ -426,9 +426,9 @@ class AbstractDatabaseBuilder {
             type: indexType,
             columns: [],
           } : {
-            name: indexName,
-            columns: [],
-          }
+              name: indexName,
+              columns: [],
+            }
           list.push(index)
         }
         index.columns.push(columnName)
@@ -489,6 +489,13 @@ class AbstractDatabaseBuilder {
 
   private isOneToMany(type: GraphQLObjectType, field: GraphQLField<any, any>) {
     const relationType = getObjectTypeFromList(field);
+    const dbAnnotations = parseMetadata('db', field.description)
+    const isJsonColumn = dbAnnotations?.type === 'json'
+
+    if (isJsonColumn) {
+      return false
+    }
+
     if ((relationType && relationType.name !== type.name) && !parseMetadata('manyToMany', field)) {
       return true;
     }
