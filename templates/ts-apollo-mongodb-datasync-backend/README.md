67026ab7df301e65f158446b7084ec6ace40afe5
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-06-25 16:26:01 +0530

    feat(datasync): add datasync template

---
+++ templates/ts-apollo-mongodb-datasync-backend/README.md
@@ -0,0 +1,47 @@
+## Graphback Apollo+MongoDB Data Synchronization Runtime Template
+
+Template showcases Graphback Data Synchronization capabilities using
+resolver layer created in-memory as opposed to codegeneration.
+
+Serverless example can be build as docker image with configurable volume for models. 
+This models will be processed at application at startup giving fully featured GraphQL Server 
+following Graphback CRUD specification.
+
+For more information please refer to: 
+
+### Running example
+
+The project has been created using `graphback`. Run the project using the following steps.
+
+- Start the database
+
+```
+docker-compose up -d
+```
+
+- Define your schema in the `model/datamodel.graphql` file. Or use the default:
+
+```
+type User {
+  id: ID!
+  name: String
+}
+```
+
+> **NOTE**: for Data Synchonization to work, you must use the `@datasync` annotation on required model(s)
+
+- Start the server
+
+```
+npm run develop
+```
+
+Or, if using yarn
+
+```
+yarn develop
+```
+
+For more on customising your Graphback application, check out [our docs](https://graphback.dev/docs/gettingstarted)
+
+If your project contains client application please follow `./client/README.md` for running client side application
---
+++ templates/ts-apollo-mongodb-datasync-backend/src/index.ts
@@ -0,0 +1,54 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config()
+import path from 'path'
+import http from "http"
+import { ApolloServer } from "apollo-server-express"
+import { buildGraphbackAPI } from 'graphback'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { createDataSyncMongoDbProvider, createDataSyncCRUDService, DataSyncPlugin } from '@graphback/datasync'
+// eslint-disable-next-line @typescript-eslint/tslint/config
+import cors from "cors"
+// eslint-disable-next-line @typescript-eslint/tslint/config
+import express from "express"
+import { PubSub } from "graphql-subscriptions";
+import { connectDB } from './db'
+
+async function start() {
+  const app = express()
+
+  app.use(cors())
+
+  const modelDefs = loadSchemaSync(path.resolve('./model/*.graphql'), {
+    loaders: [
+      new GraphQLFileLoader()
+    ]
+  })
+
+  const db = await connectDB()
+
+  const { typeDefs, resolvers, contextCreator } = buildGraphbackAPI(modelDefs, {
+    dataProviderCreator: createDataSyncMongoDbProvider(db, { Comment: 1 }, { pruneInterval: '10 seconds' }),
+    serviceCreator: createDataSyncCRUDService({ pubSub: new PubSub() }),
+    plugins: [
+      new DataSyncPlugin()
+    ]
+  });
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: contextCreator
+  })
+
+  apolloServer.applyMiddleware({ app })
+
+  const httpServer = http.createServer(app)
+  apolloServer.installSubscriptionHandlers(httpServer)
+
+  httpServer.listen({ port: 4000 }, () => {
+    console.log(`🚀  Server ready at http://localhost:4000/graphql`)
+  })
+}
+
+start().catch((err: any) => console.log(err))
