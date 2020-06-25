d01c2f03f2312bb047bee226005b1d6cf02b36a5
Enda Phelan <ephelan@redhat.com>
2020-06-25 12:41:27 +0100

    feat: add helper to clone wrapping type

---
+++ packages/graphback-codegen-schema/src/definitions/copyWrappingType.ts
@@ -0,0 +1,35 @@
+import { isWrappingType, GraphQLNonNull, isListType, GraphQLList, GraphQLType, getNamedType } from 'graphql';
+
+type WrappingType = 'GraphQLList' | 'GraphQLNonNull'
+
+/**
+ * Copies the wrapping type(s) from one GraphQLType to another
+ *
+ * @param {GraphQLType} copyFromType - Get the wrapping types from this type
+ * @param {GraphQLType} copyToType - Add the wrapping types to this type
+ */
+export function copyWrappingType(copyFromType: GraphQLType, copyToType: GraphQLType): GraphQLType {
+  const wrappers: WrappingType[] = []
+
+  let oldTypeCopy = copyFromType
+  while (isWrappingType(oldTypeCopy)) {
+    if (isListType(oldTypeCopy)) {
+      wrappers.push('GraphQLList')
+    } else {
+      wrappers.push('GraphQLNonNull')
+    }
+    oldTypeCopy = oldTypeCopy.ofType
+  }
+
+  let namedNewType: GraphQLType = getNamedType(copyToType)
+  while (wrappers.length > 0) {
+    const wrappingType = wrappers.pop()
+    if (wrappingType === 'GraphQLList') {
+      namedNewType = GraphQLList(namedNewType)
+    } else {
+      namedNewType = GraphQLNonNull(namedNewType)
+    }
+  }
+
+  return namedNewType
+}
