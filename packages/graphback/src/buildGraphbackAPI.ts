086ad28d6bc183c3f1614fbc53e7571e6e4b29ca
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-08-03 11:02:52 +0200

    fix: make sure that `options` is initialised in Graphback context
    Fixes https://github.com/aerogear/graphback/issues/1813

--- packages/graphback/src/buildGraphbackAPI.ts
+++ packages/graphback/src/buildGraphbackAPI.ts
@@ -124,7 +124,7 @@ export function buildGraphbackAPI(model: string | GraphQLSchema, config: Graphba
   const contextCreator = (context: any) => {
     return {
       ...context,
-      graphback: { services }
+      graphback: { services, options: {} }
     }
   }
 
