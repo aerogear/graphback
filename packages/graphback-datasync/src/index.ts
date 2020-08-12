c447861829eb047e6d0537db59d0e0f140e1e33d
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-08-12 13:16:17 +0530

    test(datasync): add integration test for datasync-conflict

--- packages/graphback-datasync/src/index.ts
+++ packages/graphback-datasync/src/index.ts
@@ -3,6 +3,7 @@ export * from './providers';
 export * from './services';
 export * from './util';
 export * from './helpers/createDataSync';
+export * from './deltaSource';
 
 //Required for plugins
 export { DataSyncPlugin as Plugin } from './DataSyncPlugin'
