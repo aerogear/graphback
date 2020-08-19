2dd562b703fbb279b06551bb30c055cccdae6864
Shibani Shankar Dash <shibanidash71@gmail.com>
2020-08-19 17:01:28 +0530

    feat(graphqlserve): add conflicts to --datasync

--- packages/graphql-serve/src/runtime.ts
+++ packages/graphql-serve/src/runtime.ts
@@ -1,12 +1,20 @@
 
-import { GraphbackAPI, buildGraphbackAPI } from 'graphback'
+import { GraphbackAPI, buildGraphbackAPI, filterModelTypes } from 'graphback'
 import { createMongoDbProvider } from '@graphback/runtime-mongo'
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { MongoClient, Db } from 'mongodb';
 import { loadModel } from './loadModel';
-import { GraphQLSchema } from 'graphql';
-import { DataSyncPlugin, createDataSyncMongoDbProvider, createDataSyncCRUDService } from "@graphback/datasync"
-import { PubSub } from 'graphql-subscriptions';
+import { GraphQLSchema, GraphQLObjectType } from 'graphql';
+import { createDataSyncAPI, DataSyncModelConfigMap, ClientSideWins, ServerSideWins, ConflictResolutionStrategy, isDataSyncType } from "@graphback/datasync"
+
+
+export type ConflictResolutionStrategyName = "clientSideWins"|"serverSideWins";
+
+export interface DataSyncServeConfig {
+  datasync: boolean,
+  conflict?: ConflictResolutionStrategyName,
+  deltaTTL?: number
+}
 
 export interface Runtime {
   schema: GraphQLSchema;
@@ -25,19 +33,48 @@ export const createMongoDBClient = async (): Promise<MongoClient> => {
   return client
 }
 
+export function getConflictMapForModels(model: GraphQLSchema, conflictResolutionStrategy: ConflictResolutionStrategy, deltaTTL: number): DataSyncModelConfigMap {
+  const map: DataSyncModelConfigMap = {};
+
+  const dataSyncModelTypes = filterModelTypes(model).filter(isDataSyncType);
+
+  dataSyncModelTypes.forEach((type: GraphQLObjectType) => {
+    map[type.name] = {
+      enabled: true,
+      deltaTTL: deltaTTL,
+      conflictResolution: conflictResolutionStrategy
+    }
+  })
+
+  return map
+}
+
 /**
  * Method used to create runtime schema
  * It will be part of the integration tests
  */
-export const createRuntime = async (modelDir: string, db: Db, datasync: boolean): Promise<GraphbackAPI> => {
+export const createRuntime = async (modelDir: string, db: Db, datasyncServeConfig: DataSyncServeConfig): Promise<GraphbackAPI> => {
   const model = await loadModel(modelDir);
 
   let graphbackAPI;
-  if (datasync) {
-    graphbackAPI = buildGraphbackAPI(model, {
-      serviceCreator: createDataSyncCRUDService({ pubSub: new PubSub() }),
-      dataProviderCreator: createDataSyncMongoDbProvider(db),
-      plugins: [new DataSyncPlugin()]
+  if (datasyncServeConfig.datasync) {
+    let conflictResolutionStrategy = ClientSideWins;
+    let deltaTTL = 172800;
+    if (datasyncServeConfig.conflict === "clientSideWins") {
+      conflictResolutionStrategy = ClientSideWins;
+    }
+    if (datasyncServeConfig.conflict === "serverSideWins") {
+      conflictResolutionStrategy = ServerSideWins;
+    }
+
+    if (datasyncServeConfig.deltaTTL !== undefined && datasyncServeConfig.deltaTTL !== null) {
+      deltaTTL = datasyncServeConfig.deltaTTL
+    }
+    const modelConflictMap = getConflictMapForModels(model, conflictResolutionStrategy, deltaTTL);
+
+    graphbackAPI = createDataSyncAPI(model, {
+      db,
+      dataSyncConflictMap: modelConflictMap
     })
 
   } else {
