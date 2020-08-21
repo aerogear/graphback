3d57eeb622e0cafcbca7018835a2ac39d4503375
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-08-21 15:17:52 +0200

    chore(lint): disable "unsafe return" rule in graphql-serve printSchemaHandler

--- packages/graphql-serve/src/components/printSchemaHandler.ts
+++ packages/graphql-serve/src/components/printSchemaHandler.ts
@@ -5,6 +5,7 @@ import { SchemaCRUDPlugin } from '@graphback/codegen-schema';
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 export function printSchemaHandler (argv: { model?: string }): string {
   const schemaText = loadModel(argv.model);
 
@@ -18,10 +19,9 @@ export function printSchemaHandler (argv: { model?: string }): string {
 
   const schema = metadata.getSchema();
 
-  const printedSchema: string = printSchemaWithDirectives(schema)
-
-  return printedSchema
+  return printSchemaWithDirectives(schema);
 }
 /* eslint-enable @typescript-eslint/no-unsafe-assignment */
 /* eslint-enable @typescript-eslint/no-unsafe-member-access */
 /* eslint-enable @typescript-eslint/no-unsafe-call */
+/* eslint-enable @typescript-eslint/no-unsafe-return */
