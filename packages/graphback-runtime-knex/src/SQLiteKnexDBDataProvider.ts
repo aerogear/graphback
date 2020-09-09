2173f77b1d0af5964f639c56db2b25dbbebf6027
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-09-09 17:53:46 +0200

    refactor(knex): use `returning` to avoid sending multiple queries to the database
    For SQLite we've no option since `returning` is not supported

--- packages/graphback-runtime-knex/src/SQLiteKnexDBDataProvider.ts
+++ packages/graphback-runtime-knex/src/SQLiteKnexDBDataProvider.ts
@@ -26,4 +26,32 @@ export class SQLiteKnexDBDataProvider<Type = any> extends KnexDBDataProvider<Typ
     }
     throw new NoDataError(`Cannot create ${this.tableName}`);
   }
+
+  public async update(data: Partial<Type>, selectedFields?: string[]): Promise<Type> {
+    const { idField, data: updateData } = getDatabaseArguments(this.tableMap, data);
+
+    //tslint:disable-next-line: await-promise
+    const updateResult = await this.db(this.tableName).update(updateData).where(idField.name, '=', idField.value);
+    if (updateResult === 1) {
+      //tslint:disable-next-line: await-promise
+      const dbResult = await this.db.select(this.getSelectedFields(selectedFields)).from(this.tableName).where(idField.name, '=', idField.value);
+      if (dbResult && dbResult[0]) {
+        return dbResult[0]
+      }
+    }
+    throw new NoDataError(`Cannot update ${this.tableName}`);
+  }
+
+  public async delete(data: Partial<Type>, selectedFields?: string[]): Promise<Type> {
+    const { idField } = getDatabaseArguments(this.tableMap, data);
+
+    //tslint:disable-next-line: await-promise
+    const beforeDelete = await this.db.select(this.getSelectedFields(selectedFields)).from(this.tableName).where(idField.name, '=', idField.value);
+    //tslint:disable-next-line: await-promise
+    const dbResult = await this.db(this.tableName).where(idField.name, '=', idField.value).del()
+    if (dbResult && beforeDelete[0]) {
+      return beforeDelete[0];
+    }
+    throw new NoDataError(`Cannot delete ${this.tableName} with ${JSON.stringify(data)}`);
+  }
 }
