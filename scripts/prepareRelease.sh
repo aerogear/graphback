eff38109e9bd788700c261efcb8018c26132f158
Enda Phelan <ephelan@redhat.com>
2020-10-06 13:09:23 +0100

    build(changelog): add a changelog generator

--- scripts/prepareRelease.sh
+++ scripts/prepareRelease.sh
@@ -11,5 +11,14 @@ if [ ! "$CI" = true ]; then
   yarn lerna publish --skip-git --force-publish=* --skip-npm --exact
 fi
 
+if [[ -z "${GITHUB_AUTH}" ]]; then
+  echo "\033[0;33mWarning: GitHub Token not set for lerna-changelog package. See https://github.com/lerna/lerna-changelog#github-token\033[0m"
+fi
+
+# print CHANGELOG from PRs
+yarn lerna-changelog
+
+echo "Latest changelog has been printed to your console"
+
 echo "Repository is ready for release."
 
