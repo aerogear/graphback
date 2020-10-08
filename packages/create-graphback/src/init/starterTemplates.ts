32d556d2283ee1881bc3cdfe53d2ae50e35bfabf
Enda Phelan <ephelan@redhat.com>
2020-10-08 16:33:03 +0100

    feat(templates): add kafka template

--- packages/create-graphback/src/init/starterTemplates.ts
+++ packages/create-graphback/src/init/starterTemplates.ts
@@ -103,8 +103,8 @@ export const allTemplates: Template[] = [
     }]
   },
   {
-    name: 'apollo-mongo-apache-kafka-server-ts',
-    description: 'Apollo GraphQL server connecting to MongoDB database with Apache Kafka for subscriptions',
+    name: '[PREVIEW] apollo-mongo-apache-kafka-server-ts',
+    description: 'Apollo GraphQL server connecting to MongoDB database with Apache Kafka for subscriptions. This is a preview template - we are continuing to enhance it - we welcome your feedback.',
     repos: [{
       uri: 'https://github.com/aerogear/graphback',
       branch: 'templates-1.0.0',
