b947d2be17739a54ba78f304a418dd3b6027c183
Wojciech Trocki <wtrocki@redhat.com>
2020-12-16 16:09:47 +0000

    fix: move kafka to community templates

--- packages/create-graphback/src/init/communityTemplates.ts
+++ packages/create-graphback/src/init/communityTemplates.ts
@@ -3,4 +3,14 @@
  * Array containing extrernal templates for the Graphback
  * See contribution guide for more information
  */
-export const externalTemplates = [];
+export const externalTemplates = [
+  {
+    name: 'apollo-mongo-apache-kafka-server-ts',
+    description: 'Apollo GraphQL server connecting to MongoDB database with Apache Kafka for subscriptions. This is a preview template - we are continuing to enhance it - we welcome your feedback.',
+    repos: [{
+      uri: 'https://github.com/aerogear/graphback',
+      branch: 'templates-1.0.0',
+      path: '/templates/ts-apollo-mongo-apache-kafka-backend'
+    }]
+  }
+];
