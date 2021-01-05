a708493d07ee1d8e418fd41486d6c15e2e842c62
Arsh Sharma <arshsharma461@gmail.com>
2021-01-05 16:43:15 +0530

    fix(fastify-template): linting fix

--- templates/ts-fastify-mongodb-backend/src/index.ts
+++ templates/ts-fastify-mongodb-backend/src/index.ts
@@ -10,7 +10,7 @@ import { loadConfigSync } from 'graphql-config';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { connectDB } from './db';
 import { noteResolvers } from './resolvers/noteResolvers';
-import { mergeResolvers } from '@graphql-tools/merge';
+import {mergeResolvers} from '@graphql-tools/merge';
 
 async function start() {
   const app = fastify();
@@ -21,9 +21,9 @@ async function start() {
   const config = loadConfigSync({
     extensions: [
       () => ({
-        name: graphbackExtension,
-      }),
-    ],
+        name: graphbackExtension
+      })
+    ]
   });
 
   const projectConfig = config.getDefault();
@@ -34,20 +34,22 @@ async function start() {
   const db = await connectDB();
 
   const { typeDefs, resolvers, contextCreator } = buildGraphbackAPI(modelDefs, {
-    dataProviderCreator: createMongoDbProvider(db),
+    dataProviderCreator: createMongoDbProvider(db)
   });
 
   const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers: [resolvers, noteResolvers],
-  });
+    typeDefs, resolvers: [
+      resolvers,
+      noteResolvers
+    ]
+  })
 
-  app.register(mercurius as any, {
+  app.register((mercurius as any), {
     schema,
     resolvers: mergeResolvers([resolvers,noteResolvers]),
     graphiql: true,
     context: contextCreator,
-    subscription: true,
+    subscription: true
   });
 
   app.listen(4000, () => {
