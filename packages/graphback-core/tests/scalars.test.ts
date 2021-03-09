9cd379cbe461a5579416f556945d7d17fccd2ca0
Anton Malofeev <arenukvern@gmail.com>
2021-03-09 15:38:03 +0300

    test: should validate objectId as hex string
    test: should not validate objectId as hex string

--- packages/graphback-core/tests/scalars.test.ts
+++ packages/graphback-core/tests/scalars.test.ts
@@ -1,10 +1,46 @@
-import { GraphQLInt, GraphQLFloat, GraphQLString } from 'graphql';
-import { TimeResolver, ObjectIDResolver, JSONResolver, JSONObjectResolver } from 'graphql-scalars';
-import { isSpecifiedGraphbackScalarType, GraphbackTime, GraphbackDateTime, GraphbackTimestamp, GraphbackDate, GraphbackObjectID, GraphbackJSON, GraphbackJSONObject, isSpecifiedGraphbackJSONScalarType } from "../src";
+import { ObjectId } from 'bson';
+import { GraphQLFloat, GraphQLInt, GraphQLString } from 'graphql';
+import {
+  JSONObjectResolver,
+  JSONResolver,
+  ObjectIDResolver,
+  TimeResolver,
+} from 'graphql-scalars';
+import {
+  GraphbackDate,
+  GraphbackDateTime,
+  GraphbackJSON,
+  GraphbackJSONObject,
+  GraphbackObjectID,
+  GraphbackTime,
+  GraphbackTimestamp,
+  isSpecifiedGraphbackJSONScalarType,
+  isSpecifiedGraphbackScalarType,
+} from '../src';
+import { isObjectID } from '../src/scalars/objectId';
 
-describe("Graphback scalars", () => {
+describe('Graphback scalars', () => {
+  test('should validate objectId as hex string', () => {
+    const objectId = new ObjectId();
+    const hex = objectId.toHexString();
+    const result = isObjectID(hex);
+    expect(result).toBeTruthy();
+  });
+  test('should not validate objectId as hex string', () => {
+    const result = isObjectID('zzzzzzz');
+    expect(result).toBeFalsy();
+  });
+
   test('should return false for none GraphbackScalars', () => {
-    const scalars = [TimeResolver, ObjectIDResolver, JSONResolver, JSONObjectResolver, GraphQLInt, GraphQLFloat, GraphQLString];
+    const scalars = [
+      TimeResolver,
+      ObjectIDResolver,
+      JSONResolver,
+      JSONObjectResolver,
+      GraphQLInt,
+      GraphQLFloat,
+      GraphQLString,
+    ];
 
     for (const scalar of scalars) {
       const isGraphbackScalar = isSpecifiedGraphbackScalarType(scalar);
@@ -13,7 +49,15 @@ describe("Graphback scalars", () => {
   });
 
   test('should return true for all Graphback scalars', () => {
-    const graphbackScalars = [GraphbackTime, GraphbackDateTime, GraphbackTimestamp, GraphbackDate, GraphbackObjectID, GraphbackJSON, GraphbackJSONObject];
+    const graphbackScalars = [
+      GraphbackTime,
+      GraphbackDateTime,
+      GraphbackTimestamp,
+      GraphbackDate,
+      GraphbackObjectID,
+      GraphbackJSON,
+      GraphbackJSONObject,
+    ];
 
     for (const graphbackScalar of graphbackScalars) {
       const isGraphbackScalar = isSpecifiedGraphbackScalarType(graphbackScalar);
@@ -25,17 +69,30 @@ describe("Graphback scalars", () => {
     const graphbackJSONScalars = [GraphbackJSON, GraphbackJSONObject];
 
     for (const graphbackJSONScalar of graphbackJSONScalars) {
-      const isGraphbackJSONScalar = isSpecifiedGraphbackJSONScalarType(graphbackJSONScalar);
+      const isGraphbackJSONScalar = isSpecifiedGraphbackJSONScalarType(
+        graphbackJSONScalar,
+      );
       expect(isGraphbackJSONScalar).toBeTruthy();
     }
   });
 
   test('should return true for all non Graphback JSON scalars', () => {
-    const scalars = [GraphbackTime, GraphbackDateTime, GraphbackTimestamp, GraphbackDate, GraphbackObjectID, GraphQLFloat, GraphQLString, ObjectIDResolver, JSONObjectResolver, JSONResolver];
+    const scalars = [
+      GraphbackTime,
+      GraphbackDateTime,
+      GraphbackTimestamp,
+      GraphbackDate,
+      GraphbackObjectID,
+      GraphQLFloat,
+      GraphQLString,
+      ObjectIDResolver,
+      JSONObjectResolver,
+      JSONResolver,
+    ];
 
     for (const scalar of scalars) {
       const isGraphbackJSONScalar = isSpecifiedGraphbackJSONScalarType(scalar);
       expect(isGraphbackJSONScalar).toBeFalsy();
     }
   });
-})
+});
