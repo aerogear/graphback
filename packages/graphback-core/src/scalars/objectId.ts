85d239853f431c6667e2562df26f0c83eaebe36f
Anton Malofeev <arenukvern@gmail.com>
2021-03-09 15:16:43 +0300

    fix: objectId scalar check - isObjectID

--- packages/graphback-core/src/scalars/objectId.ts
+++ packages/graphback-core/src/scalars/objectId.ts
@@ -1,13 +1,28 @@
-import { ObjectID as BsonObjectID } from "bson";
+import { ObjectID as BsonObjectID } from 'bson';
 
 /* eslint-disable */
 export function isObjectID(value: any) {
-  if (value instanceof BsonObjectID) {
-    return true;
-  }
+  const isBsonObjectId = (val: any) => val instanceof BsonObjectID;
+  if (isBsonObjectId(value)) return true;
+
+  let isBsonExtObjectID: (val: any) => boolean = () => false;
+
   try {
     const BsonExtObjectID = require('bson-ext').ObjectID;
-    return value instanceof BsonExtObjectID;
+    isBsonExtObjectID = (val: any) => val instanceof BsonExtObjectID;
+    if (isBsonExtObjectID(value)) return true;
+  } catch {}
+
+  // value can be number or string, try to parse
+  try {
+    switch (typeof value) {
+      case 'string':
+      case 'number':
+        const parsedObjectId = parseObjectID(value);
+        if (isBsonObjectId(parsedObjectId) || isBsonExtObjectID(parsedObjectId))
+          return true;
+        break;
+    }
   } catch {}
 
   return false;
